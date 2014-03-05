2897340ef78f3ccfa9dd2f5aab5a06790af96ad4
Denys Pavlov <denys@viafoura.com>
2014-03-05 14:59:53 -0500

    touched up some bins

--- bin/timeline.sh
+++ bin/timeline.sh
@@ -2,4 +2,11 @@
 
 cd "/home/meatcar/Dropbox/Pics/webcam"
 
-mplayer tv:// -tv driver=v4l2:width=640:height=480:device=/dev/video0 -frames 1 -vf screenshot -vo "png:z=9:prefix=`date +%s`"
+IMAGE=`date +%s`
+
+sleep 1
+
+mplayer tv:// -tv driver=v4l2:width=640:height=480:device=/dev/video0 -frames 1 -vf screenshot -vo "png:z=9:prefix=$IMAGE"
+mv $IMAGE* $IMAGE.png
+
+IMAGE="$IMAGE.png"
