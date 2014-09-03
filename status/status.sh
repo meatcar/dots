a5f09ce98f904d176774dd15d64e39a370f6ecfb
Denys Pavlov <denys.pavlov@gmail.com>
2014-09-03 14:36:08 -0400

    Speed up network display (all link speeds detected in parallel)

--- status/status.sh
+++ status/status.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 ICONS="/home/meatcar/dots/status/xbm8x8"
-fg='#3B95BD'
-bg='#001E28'
+fg='#516DA8'
+bg='#D7DCE7'
 
 function netstatus {
-    link=`ip link | grep ' UP ' | awk '{print $2}' | tr -d ':'`
-    if [ -z "$link" ]; then
-        link=down;
+    links=`ip link | grep 'BROADCAST' | awk '{print $2}' | tr -d ':'`
+    if [ -z "$links" ]; then
+        echo down
+    else
+        echo $links
     fi
 }
 
@@ -15,48 +17,74 @@ function netspeed {
     # displays download / upload speed by checking the /proc/net/dev with
     # 2 second delay
     #
-    netstatus
-    dev="enp0s3"
-    net=""
-    if [ "$link" == "down" ]; then
-        net="^bg($bg) ^fg(red)^i($ICONS/info_01)Network Down^fg() ^bg()"
-        echo "^ca(1, urxvtc -e wicd-curses)$net^ca()"
-        exit
-    elif [ "$link" == "$dev" ]; then
-        net="^fg()^i($ICONS/net_wired.xbm)^fg()"
-    else
-        # access point name
-        ap=`iwconfig "$link" | grep "$dev"| awk '{ print $4 }' | sed -e 's/.*"\(.*\)"/\1/'`
-        if [ "$ap" == "extensions." ]; then
-            ap="$link"
-        fi
-        net="^fg()^i($ICONS/wifi_01.xbm)^fg() '$ap'"
-    fi
 
-    ip=`ip addr show dev "$dev"| grep 'inet '| awk '{print $2}' | sed 's;/.*$;;'`
+    net=""
+
+    links=""
+    for link in $(netstatus); do
+        if ip link show "$link" | head -n1 | grep -v 'UP' > /dev/null
+        then
+            net="$net ^bg($bg) ^fg(red)^i($ICONS/info_01) $link ^fg() ^bg()"
+            continue
+        fi
+        links="$links $link"
+    done
 
     # get the up/down speed
-    old_state=$(cat /proc/net/dev | grep $link)
-    sleep 1
-    new_state=$(cat /proc/net/dev | grep $link)
+    old=""
+    for link in $links; do
+        old_state=$(cat /proc/net/dev | grep $link)
+        old_dn=`echo ${old_state/*:/} | awk -F " " '{ print $1 }'`
+        old_up=`echo ${old_state/*:/} | awk -F " " '{ print $9 }'`
+        old="$old $old_dn;$old_up"
+    done;
 
-    old_dn=`echo ${old_state/*:/} | awk -F " " '{ print $1 }'`
-    new_dn=`echo ${new_state/*:/} | awk -F " " '{ print $1 }'`
-    dnload=$((${new_dn} - ${old_dn}))
+    sleep 0.5
 
-    old_up=`echo ${old_state/*:/} | awk -F " " '{ print $9 }'`
-    new_up=`echo ${new_state/*:/} | awk -F " " '{ print $9 }'`
-    upload=$((${new_up} - ${old_up}))
+    new=""
+    for link in $links; do
+        new_state=$(cat /proc/net/dev | grep $link)
+        new_dn=`echo ${new_state/*:/} | awk -F " " '{ print $1 }'`
+        new_up=`echo ${new_state/*:/} | awk -F " " '{ print $9 }'`
+        new="$new $new_dn;$new_up"
+    done
 
-    d_speed=$(echo "scale=0;${dnload}/1024" | bc -lq)
-    u_speed=$(echo "scale=0;${upload}/1024" | bc -lq)
+    count=1
+    for link in $links; do
+        # calculate speed
+        old_state=$(echo $old | awk -F " " "{ print \$$count }")
+        old_dn=$(echo $old_state | awk -F ";" '{ print $1 }')
+        old_up=$(echo $old_state | awk -F ";" '{ print $2 }')
 
-    down="^fg()^i($ICONS/net_down_03.xbm)^fg()${d_speed}K"
-    up="^fg()^i($ICONS/net_up_03.xbm)^fg()${u_speed}K"
+        new_state=$(echo $new | awk -F " " "{ print \$$count }")
+        new_dn=$(echo $new_state | awk -F ";" '{ print $1 }')
+        new_up=$(echo $new_state | awk -F ";" '{ print $2 }')
 
-    net=" $net $ip $down $up "
-    net="^ca(1,urxvtc -e wicd-curses)$net^ca()"
-    net="^bg($bg)$net^bg()"
+        dnload=$((${new_dn} - ${old_dn}))
+
+        upload=$((${new_up} - ${old_up}))
+
+        d_speed=$(echo "scale=0;${dnload}*2/1024" | bc -lq)
+        u_speed=$(echo "scale=0;${upload}*2/1024" | bc -lq)
+
+        
+        # Build str
+
+        str="^fg()^i($ICONS/net_wired.xbm) $link^fg()"
+
+        down="^fg()^i($ICONS/net_down_03.xbm)^fg()${d_speed}K"
+        up="^fg()^i($ICONS/net_up_03.xbm)^fg()${u_speed}K"
+
+        ip=`ip addr show dev "$link"| grep 'inet '| awk '{print $2}' | sed 's;/.*$;;'`
+
+        str="$str $ip $down $up "
+        str="^ca(1,urxvtc -e wicd-curses)$str^ca()"
+        str="^bg($bg)$str^bg()"
+
+        net="$net      $str"
+
+        count=$(expr $count + 1)
+    done
 
     echo "$net"
 }
@@ -116,16 +144,16 @@ function volume {
     fi
     vol=" $vol "
     vol="^ca(1,urxvtc -e alsamixer)$vol^ca()"
-    vol="^ca(2,amixer set 'Master' 'toggle')$vol^ca()"
-    vol="^ca(4,amixer set 'Master' 5%+ )$vol^ca()"
-    vol="^ca(5,amixer set 'Master' 5%- )$vol^ca()"
+    vol="^ca(2,amixer set 'Master' 'toggle' >/dev/null)$vol^ca()"
+    vol="^ca(4,amixer set 'Master' 5%+ >/dev/null)$vol^ca()"
+    vol="^ca(5,amixer set 'Master' 5%- >/dev/null)$vol^ca()"
     vol="^bg($bg)$vol^bg()"
     echo "$vol"
 }
 
 function date_time {
-    d=`date +'^fg(#4d6080)%a %d %b^fg() %H:%M'`
+    d=`date +'^fg(#4d6080)%a %d %b^fg() %l:%M%P'`
     echo "^bg($bg) $d ^bg()"
 }
 
-echo "$(netspeed)  $(batt)  $(volume)  $(date_time)"
+echo "$(netspeed)  $(volume)  $(date_time)"
