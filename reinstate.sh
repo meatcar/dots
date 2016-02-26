36f21a0a39ed0069636738213f4f13cc19ea5ef0
Denys Pavlov <denys.pavlov@gmail.com>
2016-02-26 16:11:40 -0500

    fixing dots

--- reinstate.sh
+++ reinstate.sh
@@ -1,16 +1,14 @@
 #!/bin/sh 
 ##############################################################################
 # Reinstate all the dotfiles into their functional locations.
-#
-# NOTE: make sure and the DOTS variable is correct.
 ##############################################################################
-DOTS="$PWD"
+
 ln -rs 'ackrc' ~/'.ackrc'
 ln -rs 'bashrc' ~/'.bashrc'
 ln -rs 'bin' ~/'bin'
 ln -rs 'bspwm' ~/'.config/bspwm'
 ln -rs 'dircolors' ~/'.dircolors'
-ln -rs 'dunst' /home/meatcar/'.config/dunst'
+ln -rs 'dunst' /'.config/dunst'
 ln -rs 'fonts' ~/'.fonts'
 ln -rs 'gitconfig' ~/'.gitconfig'
 ln -rs 'htoprc' ~/'.htoprc'
@@ -21,20 +19,20 @@ ln -rs 'login' ~/'.login'
 ln -rs 'mpd' ~/'.config/mpd'
 ln -rs 'mplayer' ~/'.mplayer'
 ln -rs 'ncmpcpp' ~/'.ncmpcpp'
-ln -rs 'npmrc' /home/meatcar/'.npmrc'
+ln -rs 'npmrc' /'.npmrc'
 ln -rs 'pentadactyl' ~/'.pentadactyl'
 ln -rs 'pentadactylrc' ~/'.pentadactylrc'
 ln -rs 'screenrc' ~/'.screenrc'
-ln -rs 'spacemacs' /home/meatcar/'.spacemacs'
+ln -rs 'spacemacs' /'.spacemacs'
 ln -rs 'sxhkd' ~/'.config/sxhkd'
-ln -rs 'systemd_user' /home/meatcar/'.config/systemd'
-ln -rs 'twmn' /home/meatcar/'.config/twmn'
+ln -rs 'systemd_user' /'.config/systemd'
+ln -rs 'twmn' /'.config/twmn'
 ln -rs 'uzbl/config' ~/'.config/uzbl'
 ln -rs 'uzbl/local' ~/'.local/share/uzbl'
 ln -rs 'vimrc' ~/'.vimrc'
 ln -rs 'vim' ~/'.vim'
-ln -rs 'weechat' /home/meatcar/'.weechat'
+ln -rs 'weechat' /'.weechat'
 ln -rs 'Xdefaults' ~/'.Xdefaults'
 ln -rs 'xinitrc' ~/'.xinitrc'
-ln -rs '~/.xsessionrc' ~/'.login'
+ln -rs ~/'.xsessionrc' ~/'.login'
 ln -rs 'zshrc' ~/'.zshrc'
