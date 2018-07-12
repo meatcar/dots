e6ac2bcf5f8267953ed3d5e75f64931ce3fc88aa
Denys Pavlov <denys.pavlov@gmail.com>
2018-07-12 13:19:29 -0400

    colors

--- colors/colors.sh
+++ colors/colors.sh
@@ -1,7 +1,15 @@
+export COLORSCHEME_DIR="/home/meatcar/dots/colors/templates"
 export COLORSCHEME_NAME="base16"
-export COLORSCHEME_TYPE="flat"
+export COLORSCHEME_TYPE="dracula"
 export COLORSCHEME_LIGHT="dark"
-export COLORSCHEME="$COLORSCHEME_NAME-$COLORSCHEME_TYPE.$COLORSCHEME_LIGHT"
-export COLORSCHEME_DIR="/home/meatcar/dots/colors/$COLORSCHEME_NAME/"
+COLORSCHEME="$COLORSCHEME_NAME-$COLORSCHEME_TYPE.$COLORSCHEME_LIGHT"
+if [ -f "$COLORSCHEME_DIR"/shell/scripts/"$COLORSCHEME".sh ]; then
+    export COLORSCHEME
+else
+    export COLORSCHEME="$COLORSCHEME_NAME-$COLORSCHEME_TYPE"
+fi
 
-ln -sf "$COLORSCHEME_DIR"/vim/"$COLORSCHEME_NAME"-"$COLORSCHEME_TYPE".vim /home/meatcar/.vim/colors/meatcar.vim
+if readlink ~/.vim/colors/meatcar.vim | grep -v -E "$COLORSCHEME.vim$" >/dev/null 2>&1;
+then
+    ln -sf "$COLORSCHEME_DIR"/vim/colors/"$COLORSCHEME".vim /home/meatcar/.vim/colors/meatcar.vim
+fi
