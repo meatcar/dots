c5ddd490fdc12f8e5e89bd44fd2e9de678494b9b
Denys Pavlov <denys.pavlov@gmail.com>
2019-02-07 15:03:11 -0500

    pretty fish is best fish

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,8 @@
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
+
+set fish_greeting
+source ~/.asdf/asdf.fish
