9b246ad6f22d830a46ae847344ed905bc26b9738
Denys Pavlov <me@denys.me>
2019-09-25 07:07:59 +0800

    nix: add kakoune, misc tools

--- nix/.config/nixpkgs/home.nix
+++ nix/.config/nixpkgs/home.nix
@@ -11,11 +11,25 @@
     isync
     msmtp
     ripgrep
+    jq
   ];
   home.sessionVariables.XDG_RUNTIME_DIR = "/var/run/user/$UID";
   xdg.enable = true;
+  fonts.fontconfig.enable = true;
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+  programs.man.enable = true;
+  programs.kakoune = {
+    enable = true;
+    config = {
+      colorScheme = "palenight";
+      showMatching = true;
+      ui = {
+        enableMouse = true;
+        assistant = "none";
+      };
+    };
+  };
 
   imports = [
     ./pkgs/git
