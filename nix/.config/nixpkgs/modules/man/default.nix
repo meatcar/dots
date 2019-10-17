fa2724a559a39fc9a2f7396f395f7aa57d117a64
Denys Pavlov <me@denys.me>
2019-10-18 07:27:46 +0800

    fix(nixos): manpath shouldnt be set for root

--- nix/.config/nixpkgs/modules/man/default.nix
+++ nix/.config/nixpkgs/modules/man/default.nix
@@ -1,15 +1,15 @@
 { config, ... }:
 let
   inherit (builtins) getEnv;
-  user = getEnv "USER";
+  user = config.home.username;
 in
 {
   programs.man.enable = true;
   # Fix mandb as per https://github.com/NixOS/nixpkgs/pull/18521
   home.file.".manpath" = {
     text = ''
-      MANPATH_MAP /home/${user}/.nix-profile/bin   /home/${user}/.nix-profile/share/man
-      MANPATH_MAP /home/${user}/.nix-profile/sbin  /home/${user}/.nix-profile/share/man
+      MANPATH_MAP /home/${user}/.nix-profile/bin       /home/${user}/.nix-profile/share/man
+      MANPATH_MAP /home/${user}/.nix-profile/sbin      /home/${user}/.nix-profile/share/man
       MANDB_MAP   /home/${user}/.nix-profile/share/man ${config.xdg.cacheHome}/man
     '';
     onChange = "mkdir -p $XDG_CACHE_HOME/man && mandb -u";
