e2567474fb769f73af4591518f3e1b5018febf5d
Denys Pavlov <me@denys.me>
2019-10-26 23:10:50 +0800

    fix(nix): nano not getting set

--- nix/.config/nixpkgs/systems/wsl.nix
+++ nix/.config/nixpkgs/systems/wsl.nix
@@ -1,6 +1,7 @@
-{ pkgs, lib, ... }:
+{ config, lib, pkgs, ... }:
 {
-  home.sessionVariables = { XDG_RUNTIME_DIR = "$HOME/.cache/runtime"; };
+  home.sessionVariables = config.home.sessionVariables // { XDG_RUNTIME_DIR = "$HOME/.cache/runtime"; };
+
   home.packages = builtins.attrValues {
     inherit (pkgs) keybase kbfs;
   };
