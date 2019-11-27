4753a4f1b46d0749869c5183eeab84bc5f18163a
Denys Pavlov <me@denys.me>
2019-11-28 01:56:40 +0800

    fix(nix): infinite recursion on wsl

--- nix/.config/nixpkgs/systems/wsl.nix
+++ nix/.config/nixpkgs/systems/wsl.nix
@@ -1,12 +1,12 @@
 { config, lib, pkgs, ... }:
 {
-  home.sessionVariables = config.home.sessionVariables // { XDG_RUNTIME_DIR = "$HOME/.cache/runtime"; };
+  home.sessionVariables = { XDG_RUNTIME_DIR = "$HOME/.cache/runtime"; };
 
   home.packages = builtins.attrValues {
     inherit (pkgs) keybase kbfs;
   };
 
-  xdg.configFile."fish/fishfile".text = config.xdg.configFile."fish/fishfile".text + ''
+  xdg.configFile."fish/fishfile".text = ''
     # WSL Only:
     danhper/fish-ssh-agent
   '';
