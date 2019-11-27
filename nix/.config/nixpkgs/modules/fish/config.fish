b0d83bad9cf979196015a09d61cb1c7e9362d295
Denys Pavlov <me@denys.me>
2019-11-28 02:47:42 +0800

    feat: add direnv

--- nix/.config/nixpkgs/modules/fish/config.fish
+++ nix/.config/nixpkgs/modules/fish/config.fish
@@ -55,3 +55,7 @@ set -x LESSHISTFILE "$XDG_CACHE_HOME"/less/history
 if [ -z "$SSH_AUTH_SOCK" ] && command -qs gnome-keyring-daemon
     gnome-keyring-daemon --start --components=pkcs11,secrets,ssh | fishify -x | source -
 end
+
+if command -qs direnv
+    direnv hook fish | source
+end
