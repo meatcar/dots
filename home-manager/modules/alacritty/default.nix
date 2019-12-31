8e66e6400d91268117ce8da1998f4d3893a091ac
Denys Pavlov <me@denys.me>
2019-12-31 19:35:05 +0800

    feat(nix): pull alacritty themes out into config

--- home-manager/modules/alacritty/default.nix
+++ home-manager/modules/alacritty/default.nix
@@ -1,18 +1,8 @@
 { config, pkgs, ... }:
-let
-  # previews in README.
-  alacritty-themes = config.sources.alacritty-theme;
-  # previews: http://chriskempson.com/projects/base16/
-  base16-themes = config.sources.base16-alacritty;
-  theme = {
-    light = "${base16-themes}/colors/base16-summerfruit-light-256.yml";
-    dark = "${base16-themes}/colors/base16-material-palenight-256.yml";
-  };
-in
 {
   programs.alacritty.enable = true;
   xdg.configFile."alacritty/alacritty.yml".text = ''
     ${builtins.readFile ./alacritty.yml}
-    ${builtins.readFile theme.dark}
+    ${builtins.readFile config.themes.light.alacritty}
   '';
 }
