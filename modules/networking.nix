9e01d5ef3baeb0a36b2432230f5e3cdc9df3a06b
Denys Pavlov <me@denys.me>
2020-01-11 15:46:58 +0800

    fix(nixos): use iwd instead of networkmanager

--- modules/networking.nix
+++ modules/networking.nix
@@ -1,10 +1,10 @@
 { config, lib, pkgs, ... }:
 {
   networking = {
-    # wireless.iwd.enable = true;
-    networkmanager.enable = true;
-    networkmanager.appendNameservers = [ "8.8.8.8" ];
-    nameservers = [ "127.0.0.1" ];
+    wireless.iwd.enable = true;
+    nameservers = [ "127.0.0.1" "8.8.8.8" ];
+    # fail-over on public hotspots, less secure but I'm ok with that.
+    resolvconf.extraConfig = "resolv_conf_local_only=NO\n";
   };
 
   services.dnscrypt-proxy.enable = false;
