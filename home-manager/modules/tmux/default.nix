117ad19671cb7614ef14a247280ae4387481dc1a
Denys Pavlov <me@denys.me>
2020-01-11 15:45:35 +0800

    fix(nix): nixify tmux

--- home-manager/modules/tmux/default.nix
+++ home-manager/modules/tmux/default.nix
@@ -1,8 +1,41 @@
-{ pkgs, ... }: {
+{ config, pkgs, ... }:
+let
+  inherit (pkgs) tmuxPlugins;
+in
+{
   programs.tmux = {
     enable = true;
     extraConfig = builtins.readFile ./tmux.conf;
+    aggressiveResize = true;
+    baseIndex = 1;
     secureSocket = false;
+    escapeTime = 0;
+    keyMode = "vi";
+    shortcut = "a";
+    terminal = "tmux-256color";
+    plugins = [
+      tmuxPlugins.yank
+      tmuxPlugins.pain-control
+      tmuxPlugins.ctrlw
+      {
+        plugin = tmuxPlugins.resurrect;
+        extraConfig = ''
+          set -g @resurrect-process "mosh-client neomutt nvim weechat"
+        '';
+      }
+      {
+        plugin = tmuxPlugins.continuum;
+        extraConfig = ''
+          set -g @continuum-restore "on"
+        '';
+      }
+      {
+        plugin = tmuxPlugins.prefix-highlight;
+        extraConfig = ''
+          set -g @prefix_highlight_show_copy_mode "on"
+        '';
+      }
+    ];
   };
 
   home.packages = [];
