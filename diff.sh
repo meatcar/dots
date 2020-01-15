f508450c925726b612eabdce606a26a854bb480d
Denys Pavlov <me@denys.me>
2020-01-15 19:11:01 +0800

    feat(nix): add diff script

---
+++ diff.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+nox-update --quiet /run/booted-system /run/current-system \
+    | grep -v '\.drv : $' \
+    | sed 's/^ *//' \
+    | sort -u -k 2 --field-separator='-' # keep nix paths, sort by package name
