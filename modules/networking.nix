c79de2c8300c41708eafe2d1a79421f67421729b
Denys Pavlov <me@denys.me>
2020-04-02 06:00:32 +0800

    chore: niv update
    - use dnscrypt-proxy2 from nixpkgs
    - remove hardware.brightnessctl
    - use mps-youtube outside of python

--- modules/networking.nix
+++ modules/networking.nix
@@ -7,16 +7,9 @@
     resolvconf.extraConfig = "resolv_conf_local_only=NO\n";
   };
 
-  services.dnscrypt-proxy.enable = false;
-  # ideally we'd just do that ^^
-  # but as of 19.09, nixos doesn't use dnscrypt-proxy2 for that service, which
-  # causes issues on bootup. So for now, let's use the open pull request for
-  # dnscrypt-proxy2 (https://github.com/NixOS/nixpkgs/issues/43298)
-
-  imports = [ ./dnscrypt-proxy2.nix ];
   services.dnscrypt-proxy2 = {
     enable = true;
-    config = {
+    settings = {
       sources.public-resolvers = {
         urls = [ "https://download.dnscrypt.info/resolvers-list/v2/public-resolvers.md" ];
         cache_file = "public-resolvers.md";
