503b13be63ff2973fcd9a9be7de7eea9d85014ed
Denys Pavlov <me@denys.me>
2020-04-14 15:23:45 -0400

    feat: package wsl-open

--- shell.nix
+++ shell.nix
@@ -2,12 +2,12 @@ let
   sources = import ./nix/sources.nix;
   nixPath = import ./nix-path.nix;
 in
-
 { pkgs ? import sources.nixpkgs {} }:
-  pkgs.mkShell {
-    NIX_PATH = builtins.concatStringsSep ":" nixPath;
-    buildInputs = [
-      pkgs.stow
-      pkgs.niv
-    ];
-  }
+pkgs.stdenv.mkDerivation {
+  name = "dots";
+  NIX_PATH = builtins.concatStringsSep ":" nixPath;
+  buildInputs = [
+    pkgs.stow
+    pkgs.niv
+  ];
+}
