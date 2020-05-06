a01046f1fccb3f666c2f6ab5a5b447dd63827489
Denys Pavlov <me@denys.me>
2020-05-05 22:14:42 -0400

    feat: fetch wsl2 x11 DISPLAY

--- home-manager/systems/wsl.nix
+++ home-manager/systems/wsl.nix
@@ -6,12 +6,15 @@
 
   home.packages = builtins.attrValues {
     inherit (pkgs) keybase kbfs wsl-open;
+
+    get-wsl-display = pkgs.writeShellScriptBin "get-wsl-display" ''
+      # Get DISPLAY for WSL2
+      echo $(cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0
+    '';
   };
 
   xdg.configFile."fish/fishfile".text = ''
     # WSL Only:
     danhper/fish-ssh-agent
   '';
-
-
 }
