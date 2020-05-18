c912daef69f997e3c47d4f24cd82cea021fb015d
Denys Pavlov <me@denys.me>
2020-05-18 15:24:44 -0400

    feat: add a pretty nixos rebuild script

--- shell.nix
+++ shell.nix
@@ -2,12 +2,17 @@ let
   sources = import ./nix/sources.nix;
   nixPath = import ./nix-path.nix;
 in
-{ pkgs ? import sources.nixpkgs {} }:
+{ pkgs ? import sources.nixpkgs { } }:
 pkgs.stdenv.mkDerivation {
   name = "dots";
   NIX_PATH = builtins.concatStringsSep ":" nixPath;
   buildInputs = [
     pkgs.stow
     pkgs.niv
+    (pkgs.writeShellScriptBin "nixos-rebuild-pretty" ''
+      # prettier than nixos-rebuild switch
+      sudo -E nix build --no-link -f '<nixpkgs/nixos>' config.system.build.toplevel
+      sudo -E nixos-rebuild switch
+    '')
   ];
 }
