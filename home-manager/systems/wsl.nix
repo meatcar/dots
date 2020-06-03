c98544a53a369ff458f42c91a4cbfc1d4587f94f
Denys Pavlov <me@denys.me>
2020-06-03 16:29:14 -0400

    feat: alias emacs gui command in wsl2

--- home-manager/systems/wsl.nix
+++ home-manager/systems/wsl.nix
@@ -1,5 +1,4 @@
-{ config, lib, pkgs, ... }:
-{
+{ config, lib, pkgs, ... }: {
   home.sessionVariables = { XDG_RUNTIME_DIR = "$HOME/.cache/runtime"; };
 
   nixpkgs.overlays = [ (import ../../overlays/wsl-open.nix) ];
@@ -11,10 +10,18 @@
       # Get DISPLAY for WSL2
       echo $(cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0
     '';
+    emacs-x = pkgs.writeShellScriptBin "emacs-x" ''
+      env DISPLAY=$(get-wsl-display) emacs & disown
+    '';
   };
 
   xdg.configFile."fish/fishfile".text = ''
     # WSL Only:
     danhper/fish-ssh-agent
+  '';
+
+  programs.fish.shellInit = ''
+    # start cron daemon
+    [ -z (pgrep cron) ] || sudo /etc/init.d/cron start 2>&1 >/dev/null
   '';
 }
