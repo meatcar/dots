a51408d8c3e4e1e22079e6ffd504b61fe0860494
Denys Pavlov <me@denys.me>
2020-06-03 16:28:46 -0400

    feat: use delta for git diff colorizing

--- home-manager/modules/git/default.nix
+++ home-manager/modules/git/default.nix
@@ -4,11 +4,11 @@
     package = pkgs.gitAndTools.gitFull;
     userEmail = "me@denys.me";
     userName = "Denys Pavlov";
-    includes = [ { path = ./config; } ];
+    includes = [{ path = ./config; }];
   };
 
   home.packages = builtins.attrValues {
-    inherit (pkgs.gitAndTools) diff-so-fancy lab hub;
+    inherit (pkgs.gitAndTools) lab hub delta;
     inherit (pkgs) lazygit mr;
     inherit (pkgs.perl528Packages) PodPerldoc; # for mr
   };
