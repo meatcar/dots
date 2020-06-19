c46819ad8d35cc2031cbcfd8414f1199d9e9b33d
Denys Pavlov <me@denys.me>
2020-06-19 14:47:12 -0400

    feat: use fzf to switch tmux sessions

--- home-manager/modules/tmux/default.nix
+++ home-manager/modules/tmux/default.nix
@@ -38,5 +38,35 @@ in
     ];
   };
 
-  home.packages = [];
+  home.packages = [
+    (pkgs.writeScriptBin "tmux_select_session" ''
+      _tmux_select_session () (
+          local -r prompt=$1
+          local -r fmt='#{session_id}:|#S|(#{session_attached} attached)'
+          { tmux display-message -p -F "$fmt" && tmux list-sessions -F "$fmt"; } \
+              | awk '!seen[$1]++' \
+              | column -t -s'|' \
+              | fzf --reverse --prompt "$prompt> " \
+              | cut -d':' -f1
+      )
+
+      # Select selected tmux session
+      # Note: To be bound to a tmux key in from .tmux.conf
+      # Example: bind-key s run "tmux new-window -n 'Switch Session' 'bash -ci tmux_select_session'"
+      _tmux_select_session 'switch session' | xargs tmux switch-client -t
+    ''
+    )
+    (pkgs.writeScriptBin "tmux_kill_session" ''
+      _tmux_select_session 'kill session' \
+        | {
+          read -r id
+          echo "$id"
+          next=$(tmux list-sessions -F '#{session_id}' | grep -v -F "$id" | head -n1)
+          if [ -n "$next" ]; then
+            tmux switch-client -t "$next"
+          fi && tmux kill-session -t "$id"
+        }
+    ''
+    )
+  ];
 }
