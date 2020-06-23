e0ead9e8ef12f76a48234c11b8125506ab412b1f
Denys Pavlov <me@denys.me>
2020-06-23 15:30:05 -0400

    feat: install steam

---
+++ modules/steam.nix
@@ -0,0 +1,9 @@
+{ pkgs, config, ... }:
+{
+  hardware.opengl.driSupport32Bit = true;
+  hardware.opengl.extraPackages32 = with pkgs.pkgsi686Linux; [ libva ];
+  hardware.pulseaudio.support32Bit = true;
+  environment.systemPackages = with pkgs; [
+    steam
+  ];
+}
