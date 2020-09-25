2b42c4272319dc8238c510a0d4983e8f274601d3
Denys Pavlov <me@denys.me>
2020-09-25 16:43:06 -0400

    use nerdfont icons in starship prompt

--- home-manager/modules/starship/default.nix
+++ home-manager/modules/starship/default.nix
@@ -10,5 +10,31 @@
     env_var = {
       variable = "STARSHIP_SHELL";
     };
+
+    battery = {
+      full_symbol = "";
+      charging_symbol = "";
+      discharging_symbol = "";
+    };
+    aws.symbol = " ";
+    conda.symbol = " ";
+    docker.symbol = " ";
+    elixir.symbol = " ";
+    elm.symbol = " ";
+    git_branch.symbol = " ";
+    golang.symbol = " ";
+    haskell.symbol = " ";
+    hg_branch.symbol = " ";
+    java.symbol = " ";
+    julia.symbol = " ";
+    memory_usage.symbol = " ";
+    nim.symbol = " ";
+    nix_shell.symbol = " ";
+    nodejs.symbol = " ";
+    package.symbol = " ";
+    php.symbol = " ";
+    python.symbol = " ";
+    ruby.symbol = " ";
+    rust.symbol = " ";
   };
 }
