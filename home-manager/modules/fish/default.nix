81cbfe89179bbdf76354b9c391d75df0c2566ddb
Denys Pavlov <me@denys.me>
2020-10-19 20:49:35 -0400

    speed up fish on wsl

--- home-manager/modules/fish/default.nix
+++ home-manager/modules/fish/default.nix
@@ -1,12 +1,30 @@
 { pkgs, ... }: {
   imports = [ ../starship ../fzf ];
-  home.packages = [ pkgs.bat pkgs.any-nix-shell pkgs.fasd ];
+  home.packages = [ pkgs.bat pkgs.fasd ];
   home.sessionVariables = { BAT_THEME = "base16"; };
 
   programs.fish = {
     enable = true;
     promptInit = ''
-      any-nix-shell fish --info-right | source
+      # output of `any-nix-shell --info-right | source`
+      function nix-shell
+        ${pkgs.any-nix-shell}/bin/.any-nix-shell-wrapper fish $argv
+        set -gx ANY_NIX_SHELL_EXIT_STATUS $status
+      end
+      function nix
+        if test $argv[1] = run
+          set argv[1] fish
+          ${pkgs.any-nix-shell}/bin/.any-nix-run-wrapper $argv
+          set -gx ANY_NIX_SHELL_EXIT_STATUS $status
+        else
+          command nix $argv
+        end
+      end
+      function fish_right_prompt
+        ${pkgs.any-nix-shell}/bin/nix-shell-info
+        printf " "
+        set -e ANY_NIX_SHELL_EXIT_STATUS
+      end
     '';
     shellInit = ''
       ${builtins.readFile ./config.fish}
