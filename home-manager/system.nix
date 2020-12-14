ae58195352493be56bb236d2ec9f3fae4c2036c2
Denys Pavlov <me@denys.me>
2020-12-14 15:55:14 -0500

    use lib.optionals over home-grown version

--- home-manager/system.nix
+++ home-manager/system.nix
@@ -1,10 +1,8 @@
 { lib, ... }:
 let
-  inherit (builtins) getEnv stringLength;
-  conditionalPaths = condition: paths: if condition then paths else [];
+  inherit (builtins) getEnv;
 in
 {
-  imports = lib.flatten [
-    (conditionalPaths (getEnv "WSL_DISTRO_NAME" != "") [ ./systems/wsl.nix ])
-  ];
+  imports = lib.optionals (getEnv "WSL_DISTRO_NAME" != "") [ ./systems/wsl.nix ];
+
 }
