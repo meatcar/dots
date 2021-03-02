70e0491074cba010d087686e00a71b5befef5c83
Denys Pavlov <me@denys.me>
2021-03-02 05:17:41 -0500

    fix: run any-nix-shell dynamically

--- home-manager/modules/fish/default.nix
+++ home-manager/modules/fish/default.nix
@@ -1,31 +1,22 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+let
+  any-nix-shell-fish = pkgs.runCommand "any-nix-shell-fish"
+    {
+      buildInputs = with pkgs; [ which ];
+    }
+    ''
+      mkdir $out
+      ${pkgs.any-nix-shell}/bin/any-nix-shell fish --info-right > $out/any-nix-shell.fish
+    '';
+in
+{
   imports = [ ../starship ../fzf ];
   home.packages = [ pkgs.bat pkgs.fasd pkgs.fd ];
   home.sessionVariables = { BAT_THEME = "base16"; };
 
   programs.fish = {
     enable = true;
-    promptInit = ''
-      # output of `any-nix-shell --info-right | source`
-      function nix-shell
-        ${pkgs.any-nix-shell}/bin/.any-nix-shell-wrapper fish $argv
-        set -gx ANY_NIX_SHELL_EXIT_STATUS $status
-      end
-      function nix
-        if test $argv[1] = run
-          set argv[1] fish
-          ${pkgs.any-nix-shell}/bin/.any-nix-run-wrapper $argv
-          set -gx ANY_NIX_SHELL_EXIT_STATUS $status
-        else
-          command nix $argv
-        end
-      end
-      function fish_right_prompt
-        ${pkgs.any-nix-shell}/bin/nix-shell-info
-        printf " "
-        set -e ANY_NIX_SHELL_EXIT_STATUS
-      end
-    '';
+    promptInit = builtins.readFile "${any-nix-shell-fish}/any-nix-shell.fish";
     shellInit = ''
       ${builtins.readFile ./config.fish}
       eval (starship init fish)
