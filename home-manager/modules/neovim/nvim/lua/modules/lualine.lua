3c05682c245f7fb065d27871311aaaf08d828260
Denys Pavlov <me@denys.me>
2021-05-14 02:13:14 -0400

    neovim: improve lualine

--- home-manager/modules/neovim/nvim/lua/modules/lualine.lua
+++ home-manager/modules/neovim/nvim/lua/modules/lualine.lua
@@ -1,11 +1,12 @@
+---@diagnostic disable-next-line: undefined-global
 local vim = vim
 local TRANSPARENT = 'NONE'
 local function get_hl_color(hl_name)
     -- this only works with `set termguicolors`
     local exists, hl = pcall(vim.api.nvim_get_hl_by_name, hl_name, true)
-    local color = hl.foreground or hl.background
+    local color = hl.foreground and hl.background
 
-    if (exists and color ~= nil) then
+    if (exists and color) then
         return {
             foreground = ('#%06x'):format(hl.foreground),
             background = ('#%06x'):format(hl.background),
@@ -14,39 +15,43 @@ local function get_hl_color(hl_name)
         return {foreground = TRANSPARENT, background = TRANSPARENT}
     end
 end
-print(vim.inspect(get_hl_color('DiffText')))
+
+local sections = {
+    lualine_a = {'mode'},
+    lualine_b = {
+        {'branch', color = {fg = get_hl_color('DiffText').foreground}},
+    },
+    lualine_c = {
+        {'diff', symbols = {added = ' ', modified = ' ', removed = ' '}},
+        {'filename', file_status = true, path = 1, symbols = {modified = ' ', readonly = ' '}},
+    },
+    lualine_x = {
+        {'diagnostics', sources = {'nvim_lsp', 'ale'}},
+        {'filetype'},
+        {'encoding', condition=function() return vim.o.encoding ~= 'utf-8' end},
+        {'fileformat', condition=function() return vim.o.fileformat ~= 'unix' end},
+    },
+    lualine_y = {
+        'progress'
+
+    },
+    lualine_z = {'location'}
+}
 
 require('lualine').setup{
     options = {
-        theme = 'auto',
+        theme = 'tokyonight',
         section_separators = {'', ''},
-        component_separators = {'', ''},
+        component_separators = {'', ''}
     },
-    sections = {
-        lualine_a = {'mode'},
-        lualine_b = {},
-        lualine_c = {
-            {'filename', file_status = true, full_path = true, shorten = true},
-        },
-        lualine_x = {
-            {'diagnostics', { sources = {'nvim_lsp', 'ale'}}},
-            {'diff', symbols = {added = ' ', modified = ' ', removed = ' '}}
-        },
-        lualine_y = {
-            {'branch', color = {fg = get_hl_color('DiffText').foreground}},
-            {'encoding', condition=function() return vim.o.encoding ~= 'utf-8' end},
-            {'fileformat', condition=function() return vim.o.fileformat ~= 'unix' end},
-            {'filetype'}
-        },
-        lualine_z = {'progress','location'}
-    },
+    sections = sections,
     inactive_sections = {
         lualine_a = {},
-        lualine_b = {},
-        lualine_c = {'filename'},
+        lualine_b = {vim.deepcopy(sections.lualine_b[1])},
+        lualine_c = {vim.deepcopy(sections.lualine_c[2])},
         lualine_x = {},
         lualine_y = {},
-        lualine_z = {'location'}
+        lualine_z = vim.deepcopy(sections.lualine_z)
     },
     extensions = { 'nvim-tree', 'fugitive' },
 }
