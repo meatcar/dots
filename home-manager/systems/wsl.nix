f341faf9d7a5d139179df6b89cca13e631970e0c
Denys Pavlov <me@denys.me>
2021-05-18 23:14:15 -0400

    wsl: make config more clear

--- home-manager/systems/wsl.nix
+++ home-manager/systems/wsl.nix
@@ -30,17 +30,24 @@
 
   programs.fish.shellInit = ''
     # Detect startup
-    if [ -z (pgrep cron) ]
-      # Since this branch is executed once, let's clean /tmp and mount
-      sudo rm -rf /tmp/*
-      sudo mount -t tmpfs tmpfs /tmp || true
+    if not pgrep cron >/dev/null
+      if not mount | grep -q /tmp
+        # mount /tmp manually a tmpfs
+        sudo rm -rf /tmp/*
+        sudo mount -t tmpfs tmpfs /tmp || true
+      end
 
-      # Occasionally drop caches to minimize WSL2 ram usages
-      # root crontab contents:
-      # */2 * * * * sync; echo 3 > /proc/sys/vm/drop_caches; touch /root/drop_caches_last_run
-      # start cron daemon
+      # recreate symlinks for wslg
+      if not test -e /tmp/.X11-unix
+        sudo ln -s /mnt/wslg/.X11-unix /tmp/.X11-unix
+      end
+
+      if not sudo crontab -l | grep -qv 'drop_caches'
+        # Occasionally drop caches to minimize WSL2 ram usages
+        #set -l crontab '*/2 * * * * sync; echo 3 > /proc/sys/vm/drop_caches; touch /root/drop_caches_last_run'
+        #(sudo crontab -l; echo "$crontab") | sudo crontab -
+      end
       sudo /etc/init.d/cron start 2>&1 >/dev/null
-
     end
 
     # WSL thinks the shell is bash, even when running fish. Let's change it's mind.
