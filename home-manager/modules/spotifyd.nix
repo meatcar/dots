855b5f9c7499897a1358667aee33ecf3ab9a59ec
Denys Pavlov <me@denys.me>
2021-08-25 01:31:55 -0400

    spotifyd: fix config

--- home-manager/modules/spotifyd.nix
+++ home-manager/modules/spotifyd.nix
@@ -6,7 +6,7 @@
       global = {
         username = "meatcar";
         password_cmd = "${pkgs.gnome3.libsecret}/bin/secret-tool lookup app spotifyd username meatcar";
-        bitrate = "320";
+        bitrate = 320;
         cache_path = "${config.xdg.cacheHome}/spotifyd";
         backend = "pulseaudio";
       };
