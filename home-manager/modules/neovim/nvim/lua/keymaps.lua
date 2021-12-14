e1148a947249544769f4af0cad127ee6ba4fa9b8
Denys Pavlov <me@denys.me>
2021-12-14 13:18:40 -0500

    fix(neovim): lsp keymaps

--- home-manager/modules/neovim/nvim/lua/keymaps.lua
+++ home-manager/modules/neovim/nvim/lua/keymaps.lua
@@ -20,3 +20,17 @@ vim.cmd [[ autocmd vimrc FileType netrw lua me.fn.keymap_netrw() ]]
 
 -- get back to normal mode quickly
 map('t', '<Esc><Esc>', [[ <C-\><C-n> ]], { noremap = true })
+
+-- lsp maps
+function _G.me.fn.keymap_lsp_on_attach(_, bufnr)
+  bmap(bufnr, 'n', 'gr', '<Cmd>Lspsaga rename<CR>', { silent = true, noremap = true })
+  bmap(bufnr, 'n', 'gx', '<Cmd>Lspsaga code_action<CR>', { silent = true, noremap = true })
+  bmap(bufnr, 'x', 'gx', ':<C-u>Lspsaga range_code_action<CR>', { silent = true, noremap = true })
+  bmap(bufnr, 'n', 'K', '<Cmd>Lspsaga hover_doc<CR>', { silent = true, noremap = true })
+  bmap(bufnr, 'n', 'go', '<Cmd>Lspsaga show_line_diagnostics<CR>', { silent = true, noremap = true })
+  bmap(bufnr, 'n', 'gj', '<Cmd>Lspsaga diagnostic_jump_next<CR>', { silent = true, noremap = true })
+  bmap(bufnr, 'n', 'gk', '<Cmd>Lspsaga diagnostic_jump_prev<CR>', { silent = true, noremap = true })
+  bmap(bufnr, 'n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', { silent = true, noremap = true })
+  bmap(bufnr, 'n', '<C-u>', "<Cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<CR>", {})
+  bmap(bufnr, 'n', '<C-d>', "<Cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<CR>", {})
+end
