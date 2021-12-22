a8ed7c43207dc33fd019bd37d0b5ed7d29fd904b
Denys Pavlov <me@denys.me>
2021-12-22 20:20:26 +0000

    feat(nixos): add wsl-nixos config
    - refactor config layout to support multiple nixos systems

--- home-manager/systems/wsl.nix
+++ home-manager/systems/wsl.nix
@@ -15,16 +15,6 @@
     BROWSER = "${pkgs.wsl-open}/bin/wsl-open";
   };
 
-  # nixpkgs config
-  nixpkgs.overlays = [ (import ../../overlays/wsl-open.nix) ];
-  nixpkgs.config = import ../config.nix;
-  xdg.configFile."nixpkgs/config.nix".text =
-    let
-      seqToString = lib.generators.toPretty { };
-      nixpkgsConfig = lib.filterAttrs (n: v: n != "packageOverrides") config.nixpkgs.config;
-    in
-    seqToString nixpkgsConfig;
-
   home.packages = builtins.attrValues {
     inherit (pkgs) keybase kbfs wsl-open;
 
