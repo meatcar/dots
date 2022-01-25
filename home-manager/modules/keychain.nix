8eabe734a80c1d18532a55dd1e89ce7dc362202a
Denys Pavlov <me@denys.me>
2022-01-25 02:39:35 +0000

    fix: teach keychain to config bash properly

--- home-manager/modules/keychain.nix
+++ home-manager/modules/keychain.nix
@@ -1,8 +1,22 @@
-{ ... }:
+{ config, lib, ... }:
 {
   programs.keychain = {
     enable = true;
     keys = [ "id_ed25519" ];
     extraFlags = [ "--noask --quiet" ];
   };
+
+  #FIXME: https://github.com/nix-community/home-manager/issues/2256
+  programs.keychain.enableBashIntegration = false;
+  programs.bash.initExtra =
+    with lib; let
+      cfg = config.programs.keychain;
+      flags = cfg.extraFlags ++ optional (cfg.agents != [ ])
+        "--agents ${concatStringsSep "," cfg.agents}"
+        ++ optional (cfg.inheritType != null) "--inherit ${cfg.inheritType}";
+      shellCommand = "${cfg.package}/bin/keychain --eval ${concatStringsSep " " flags} ${concatStringsSep " " cfg.keys}";
+    in
+    ''
+      eval "$(SHELL=bash ${shellCommand})"
+    '';
 }
