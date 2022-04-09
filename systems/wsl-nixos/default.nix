d37afdcf6594d3a6c63ca16539599fd3d7e41469
Denys Pavlov <me@denys.me>
2022-04-09 02:11:21 +0000

    feat(wsl): update to new nixos-wsl

--- systems/wsl-nixos/default.nix
+++ systems/wsl-nixos/default.nix
@@ -1,49 +1,26 @@
-{ lib, pkgs, config, modulesPath, ... }:
-
-with lib;
-let
-  defaultUser = "meatcar";
-  syschdemd = import ./syschdemd.nix { inherit lib pkgs config defaultUser; };
-in
+{ lib, pkgs, config, modulesPath, inputs, ... }:
 {
   imports = [
+    "${modulesPath}/profiles/minimal.nix"
+    inputs.nixos-wsl.nixosModules.wsl
     ../common.nix
     ../../modules/docker.nix
   ];
 
-  # WSL is closer to a container than anything else
-  boot.isContainer = true;
-
-  environment.etc.hosts.enable = false;
-  environment.etc."resolv.conf".enable = false;
-
-  networking.dhcpcd.enable = false;
+  wsl = {
+    enable = true;
+    automountPath = "/mnt";
+    defaultUser = "meatcar";
+    startMenuLaunchers = true;
+    wslConf = {
+      network.hostname = "nixos";
+    };
+  };
 
   networking.firewall.enable = true;
 
-  users.users.${defaultUser} = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" "docker" ];
+  users.users.${config.wsl.defaultUser} = {
     shell = pkgs.fish;
+    extraGroups = [ "docker" ];
   };
-
-  users.users.root = {
-    shell = "${syschdemd}/bin/syschdemd";
-    # Otherwise WSL fails to login as root with "initgroups failed 5"
-    extraGroups = [ "root" ];
-  };
-
-  security.sudo.wheelNeedsPassword = false;
-
-  # Disable systemd units that don't make sense on WSL
-  systemd.services."serial-getty@ttyS0".enable = false;
-  systemd.services."serial-getty@hvc0".enable = false;
-  systemd.services."getty@tty1".enable = false;
-  systemd.services."autovt@".enable = false;
-
-  systemd.services.systemd-resolved.enable = false;
-  systemd.services.systemd-udevd.enable = false;
-
-  # Don't allow emergency mode, because we don't have a console.
-  systemd.enableEmergencyMode = false;
 }
