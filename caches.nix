aae4ec180e61032cfe5ac2be233f3bcec522d1ad
Denys Pavlov <me@denys.me>
2022-07-11 17:52:36 +0000

    fix: nix cache warning

--- caches.nix
+++ caches.nix
@@ -2,10 +2,10 @@
 [
   {
     name = "nix-community";
-    sha256 = "sha256:00lpx4znr4dd0cc4w4q8fl97bdp7q19z1d3p50hcfxy26jz5g21g";
+    sha256 = "sha256:1955r436fs102ny80wfzy99d4253bh2i1vv1x4d4sh0zx2ssmhrk";
   }
   {
     name = "nixpkgs-wayland";
-    sha256 = "sha256:0qi8x06vkr2138dafgg5v3h8zbg4sd2349sfqhh8yayszrgn37wv";
+    sha256 = "sha256:1fkvp6ifx6cafbfk5si9401h1z7wy6ydj3dkw3bk3dl881j5ymgh";
   }
 ]
