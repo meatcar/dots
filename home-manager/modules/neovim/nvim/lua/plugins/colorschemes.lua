41993b2ef699ae56354b5e46049f0742059e6083
Denys Pavlov <me@denys.me>
2022-08-06 20:52:10 +0000

    feat(neovim): detect system theme

--- home-manager/modules/neovim/nvim/lua/plugins/colorschemes.lua
+++ home-manager/modules/neovim/nvim/lua/plugins/colorschemes.lua
@@ -9,11 +9,22 @@ return function(use)
     end
   end
 
+  -- detect system dark mode at startup/packer sync
+  vim.cmd [[autocmd packer VimEnter * ++nested lua me.fn.autocmd_onVimEnterSetBackground()]]
+  vim.cmd [[autocmd packer User PackerCompileDone ++nested lua me.fn.autocmd_onVimEnterSetBackground()]]
+  function _G.me.fn.autocmd_onVimEnterSetBackground()
+    local theme = vim.fn.readfile '/mnt/c/Users/meatcar/.config/theme'
+    print('Setting background to ' .. theme[1])
+    vim.o.background = theme[1] -- 'dark' or 'light' or a message will be shown
+  end
+
   use 'liuchengxu/space-vim-theme'
 
   use 'NLKNguyen/papercolor-theme'
 
   use 'bluz71/vim-moonfly-colors'
+
+  use 'B4mbus/oxocarbon-lua.nvim'
 
   use {
     'https://gitlab.com/protesilaos/tempus-themes-vim', -- accessible themes
