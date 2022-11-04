455313aed429a5d5d5db536574692c86bf948a55
Denys Pavlov <me@denys.me>
2022-11-04 13:50:47 +0000

    fix(fish): don't use wsl executables for path assembly

--- home-manager/modules/fish/functions/set-fish-user-paths.fish
+++ home-manager/modules/fish/functions/set-fish-user-paths.fish
@@ -2,6 +2,12 @@ function addpath
     set -aU fish_user_paths $argv[1]
 end
 
+function command_exists
+    # make sure command is not a windows executable
+    set -l fullpath (command -s $argv[1])
+    test -n $fullpath && string match -q -r '^/mnt/c' $fullpath
+end
+
 function set-fish-user-paths \
     --description "Set fish_user_paths"
 
@@ -11,7 +17,7 @@ function set-fish-user-paths \
         addpath ~/bin
     end
 
-    if command -qs emacs
+    if command_exists emacs
         addpath $HOME/.emacs.d/bin # DOOM EMACS
     end
 
@@ -19,15 +25,15 @@ function set-fish-user-paths \
         addpath $HOME/.local/bin
     end
 
-    if command -qs yarn
+    if command_exists yarn
         addpath (yarn global bin)
     end
 
-    if command -qs npm
+    if command_exists npm
         addpath (npm bin -g)
     end
 
-    if command -qs go
+    if command_exists go
         set -x GOPATH $HOME/go
         addpath (go env GOROOT)/bin
         addpath (go env GOPATH)/bin
