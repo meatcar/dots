68c001a78823abac44a11518bf23e0250cbd7bd0
Denys Pavlov <me@denys.me>
2022-11-29 18:18:22 +0000

    feat(nvim): move barbar config to lua from global vars

--- home-manager/modules/neovim/nvim/lua/plugins/pkg_barbar.lua
+++ home-manager/modules/neovim/nvim/lua/plugins/pkg_barbar.lua
@@ -2,12 +2,13 @@ return {
   'romgrk/barbar.nvim',
   requires = { 'kyazdani42/nvim-web-devicons' },
   event = 'ColorScheme', -- hack, delay bufferline until a colorscheme is set
-  setup = function()
-    vim.g.bufferline = vim.g.bufferline or {}
-    vim.g.bufferline.auto_hide = false
-    vim.g.bufferline.closeable = false
-  end,
   config = function()
+    require('bufferline').setup {
+      animation = false,
+      auto_hide = true,
+      closable = false,
+      exclude_ft = _G.me.o.sidebars,
+    }
     vim.keymap.set('n', '[b', [[<Cmd>BufferPrev<CR>]], { silent = true })
     vim.keymap.set('n', ']b', [[<Cmd>BufferNext<CR>]], { silent = true })
   end,
