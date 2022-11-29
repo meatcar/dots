5db00bb210f1ee677bc12e7a323d32ecad381217
Denys Pavlov <me@denys.me>
2022-11-29 18:19:04 +0000

    feat(nvim): +ssr

--- home-manager/modules/neovim/nvim/lua/plugins/lsp.lua
+++ home-manager/modules/neovim/nvim/lua/plugins/lsp.lua
@@ -71,4 +71,22 @@ return function(use)
       }
     end,
   }
+
+  use {
+    'cshuaimin/ssr.nvim',
+    module = 'ssr',
+    -- Calling setup is optional.
+    config = function()
+      require('ssr').setup {
+        min_width = 50,
+        min_height = 5,
+        keymaps = {
+          close = 'q',
+          next_match = 'n',
+          prev_match = 'N',
+          replace_all = '<leader><cr>',
+        },
+      }
+    end,
+  }
 end
