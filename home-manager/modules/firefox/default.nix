f710b10c8c2772877771dfbf1d324722925fddf0
Denys Pavlov <me@denys.me>
2022-11-30 19:37:51 +0000

    feat(firefox): wayland supported by default upstream

--- home-manager/modules/firefox/default.nix
+++ home-manager/modules/firefox/default.nix
@@ -3,7 +3,7 @@
 {
   programs.firefox = {
     enable = true;
-    package = pkgs.firefox-wayland;
+    package = pkgs.firefox;
     profiles.default = {
       userChrome = builtins.readFile ./userChrome.css;
       settings = {
