4cdcc0978bc82ef16fd681dedd4e4403a5c23274
Denys Pavlov <me@denys.me>
2023-01-12 13:58:55 -0500

    feat(nvim): swap nvim-tree for oil.nvim for vinegar-like file mgmt

--- home-manager/modules/neovim/nvim/lua/plugins/utils.lua
+++ home-manager/modules/neovim/nvim/lua/plugins/utils.lua
@@ -91,19 +91,80 @@ return function(use)
       require('nvim-tree').setup {
         -- disable conflict with dirvish
         hijack_netrw = true,
-        hijack_directories = { enable = true },
         renderer = {
           highlight_git = true,
           add_trailing = true,
         },
-        view = {
-          mappings = {
-            list = {
-              { key = '<CR>', action = 'edit_in_place' },
-            },
+      }
+    end,
+  }
+
+  use { -- buffer-replacing file browser
+    'stevearc/oil.nvim',
+    requires = 'kyazdani42/nvim-web-devicons',
+    config = function()
+      require('oil').setup {
+        columns = {
+          'icon',
+          'permissions',
+          'size',
+          'mtime',
+        },
+        -- Window-local options to use for oil buffers
+        win_options = {
+          wrap = false,
+          signcolumn = 'no',
+          cursorcolumn = false,
+          foldcolumn = '0',
+          spell = false,
+          list = false,
+          conceallevel = 3,
+          concealcursor = 'n',
+        },
+        -- Restore window options to previous values when leaving an oil buffer
+        restore_win_options = true,
+        -- Skip the confirmation popup for simple operations
+        skip_confirm_for_simple_edits = false,
+        -- Keymaps in oil buffer. Can be any value that `vim.keymap.set` accepts OR a table of keymap
+        -- options with a `callback` (e.g. { callback = function() ... end, desc = "", nowait = true })
+        -- Additionally, if it is a string that matches "action.<name>",
+        -- it will use the mapping at require("oil.action").<name>
+        -- Set to `false` to remove a keymap
+        keymaps = {
+          ['g?'] = 'actions.show_help',
+          ['<CR>'] = 'actions.select',
+          ['<C-v>'] = 'actions.select_vsplit',
+          ['<C-h>'] = 'actions.select_split',
+          ['<C-t>'] = 'actions.open_terminal',
+          ['<Esc>'] = 'actions.close',
+          ['q'] = 'actions.close',
+          ['gq'] = 'actions.close',
+          ['<C-l>'] = 'actions.refresh',
+          ['-'] = 'actions.parent',
+          ['_'] = 'actions.open_cwd',
+          ['`'] = 'actions.cd',
+          ['~'] = 'actions.tcd',
+          ['g.'] = 'actions.toggle_hidden',
+        },
+        -- Set to false to disable all of the above keymaps
+        use_default_keymaps = true,
+        view_options = {
+          -- Show files and directories that start with "."
+          show_hidden = false,
+        },
+        -- Configuration for the floating window in oil.open_float
+        float = {
+          -- Padding around the floating window
+          padding = 2,
+          max_width = 0,
+          max_height = 0,
+          border = 'rounded',
+          win_options = {
+            winblend = 10,
           },
         },
       }
+      vim.keymap.set('n', '-', require('oil').open, { desc = 'Open parent directory' })
     end,
   }
 
