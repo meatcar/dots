1900376465eebd690deea0ea594f299dd0e099aa
Denys Pavlov <me@denys.me>
2023-02-22 19:25:36 -0500

    fix(wsl): override tmpfiles service until fix is upstream

--- systems/wsl-nixos/default.nix
+++ systems/wsl-nixos/default.nix
@@ -32,6 +32,27 @@
     isNormalUser = true;
   };
 
+  # TODO fix for https://github.com/nix-community/NixOS-WSL/issues/185
+  systemd.services.nixs-wsl-systemd-fix = {
+    description = "Fix the /dev/shm symlink to be a mount";
+    unitConfig = {
+      DefaultDependencies = "no";
+      Before = [ "sysinit.target" "systemd-tmpfiles-setup-dev.service" "systemd-tmpfiles-setup.service" "systemd-sysctl.service" ];
+      ConditionPathExists = "/dev/shm";
+      ConditionPathIsSymbolicLink = "/dev/shm";
+      ConditionPathIsMountPoint = "/run/shm";
+    };
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = [
+        "${pkgs.coreutils-full}/bin/rm /dev/shm"
+        "/run/wrappers/bin/mount --bind -o X-mount.mkdir /run/shm /dev/shm"
+      ];
+    };
+    wantedBy = [ "sysinit.target" ];
+  };
+
+
   # override nixos-wsl startMenuLaunchers to pull in home-manager ones
   wsl.startMenuLaunchers = false; # we do it ourselves
   system.activationScripts.copy-launchers =
