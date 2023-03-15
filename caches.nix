07be5673b58e837dfd948752c5ce178098bbc79c
Denys Pavlov <me@denys.me>
2023-03-15 00:19:33 -0400

    chore: nix flake update

--- caches.nix
+++ caches.nix
@@ -2,10 +2,10 @@
 [
   {
     name = "nix-community";
-    sha256 = "sha256:1rgbl9hzmpi5x2xx9777sf6jamz5b9qg72hkdn1vnhyqcy008xwg";
+    sha256 = "sha256:0m6kb0a0m3pr6bbzqz54x37h5ri121sraj1idfmsrr6prknc7q3x";
   }
   {
     name = "nixpkgs-wayland";
-    sha256 = "sha256:0pr9d9mvhpjf3rspqlrmz7fkr5nyhabyfib89vd6dv44xqip70gc";
+    sha256 = "sha256:11mqxlyhsqxvb6zg27819h29856bc2d6s98ihrs4ksnf20fzdx7h";
   }
 ]
