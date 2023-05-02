b1d22d19469d723d04aa250c952fdda562c774f5
Denys Pavlov <me@denys.me>
2023-05-02 13:51:30 -0400

    feat(nvim): pin nixpkgs in nightly

--- flake.nix
+++ flake.nix
@@ -9,14 +9,8 @@
       home-manager.inputs.nixpkgs.follows = "nixpkgs";
       nixpkgs-wayland.url = "github:nix-community/nixpkgs-wayland";
       nixpkgs-wayland.inputs.nixpkgs.follows = "nixpkgs";
-      neovim-nightly-overlay = {
-        url = "github:nix-community/neovim-nightly-overlay";
-        # Pin to a nixpkgs revision that doesn't have NixOS/nixpkgs#208103 yet
-        # see: https://github.com/nix-community/neovim-nightly-overlay/issues/164
-        inputs.nixpkgs.url = "github:nixos/nixpkgs?rev=fad51abd42ca17a60fc1d4cb9382e2d79ae31836";
-      };
-      # neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
-      # neovim-nightly-overlay.inputs.nixpkgs.follows = "nixpkgs";
+      neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
+      neovim-nightly-overlay.inputs.nixpkgs.follows = "nixpkgs";
       emacs-overlay.url = "github:nix-community/emacs-overlay";
       emacs-overlay.inputs.nixpkgs.follows = "nixpkgs";
       declarative-cachix.url = "github:jonascarpay/declarative-cachix";
