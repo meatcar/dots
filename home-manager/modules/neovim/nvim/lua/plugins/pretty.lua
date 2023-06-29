7e9cdd3aaa34152830e0e5cf2289601ca4ae430f
Denys Pavlov <me@denys.me>
2023-06-29 17:03:29 -0400

    fix(nvim): fix signcolumn showing duplicates

--- home-manager/modules/neovim/nvim/lua/plugins/pretty.lua
+++ home-manager/modules/neovim/nvim/lua/plugins/pretty.lua
@@ -177,13 +177,13 @@ return {
         ft_ignore = _G.me.o.sidebars,
         segments = {
           {
-            sign = { name = { 'Diagnostic' }, maxwidth = 2, auto = true },
+            sign = { name = { '.*' }, maxwidth = 2, auto = true },
             click = 'v:lua.ScSa',
           },
           { text = { builtin.foldfunc }, click = 'v:lua.ScFa' },
           { text = { builtin.lnumfunc }, click = 'v:lua.ScLa' },
           {
-            sign = { name = { '.*' }, maxwidth = 2, colwidth = 1, auto = true },
+            sign = { name = { 'GitSigns*' }, maxwidth = 2, colwidth = 1, auto = true },
             click = 'v:lua.ScSa',
           },
         },
