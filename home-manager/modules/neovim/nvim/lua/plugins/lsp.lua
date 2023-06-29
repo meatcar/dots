37aeb977ec9ff5a714292d0766e22ffc22828eec
Denys Pavlov <me@denys.me>
2023-06-29 17:02:03 -0400

    feat(nvim): use icons for lsp diagnostic signs

--- home-manager/modules/neovim/nvim/lua/plugins/lsp.lua
+++ home-manager/modules/neovim/nvim/lua/plugins/lsp.lua
@@ -1,5 +1,11 @@
 local keymaps = require 'core/keymaps'
 
+local signs = { Error = '', Warn = '', Hint = '', Info = '' }
+for type, icon in pairs(signs) do
+  local hl = 'DiagnosticSign' .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
 return {
   {
     'neovim/nvim-lspconfig',
