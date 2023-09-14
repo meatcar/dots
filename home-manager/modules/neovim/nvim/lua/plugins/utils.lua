77c98974bd1bbbdeef1fe7b5a12cc35f93d8d386
Denys Pavlov <me@denys.me>
2023-09-13 22:33:34 -0400

    feat(nvim): -firenvim

--- home-manager/modules/neovim/nvim/lua/plugins/utils.lua
+++ home-manager/modules/neovim/nvim/lua/plugins/utils.lua
@@ -355,18 +355,6 @@ return {
     end,
   },
 
-  { -- use neovim to edit browser text fields
-    'glacambre/firenvim',
-
-    -- Lazy load firenvim
-    -- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
-    cond = not not vim.g.started_by_firenvim,
-    build = function()
-      require('lazy').load { plugins = 'firenvim', wait = true }
-      vim.fn['firenvim#install'](0)
-    end,
-  },
-
   { -- open browser with various queries
     'lalitmee/browse.nvim',
     keys = {
