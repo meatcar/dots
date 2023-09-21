bb1114de77a204129cd048112bb548e86a1ad72e
Denys Pavlov <me@denys.me>
2023-09-20 23:08:41 -0400

    feat(nvim): add a bracket when completing funciton names

--- home-manager/modules/neovim/nvim/lua/plugins/base.lua
+++ home-manager/modules/neovim/nvim/lua/plugins/base.lua
@@ -171,6 +171,15 @@ return {
   {
     'windwp/nvim-autopairs',
     event = me.o.events.insert,
-    config = true
+    opts = {},
+    config = function(_, opts)
+      require('nvim-autopairs').setup(opts)
+      local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+      local cmp = require('cmp')
+      cmp.event:on(
+        'confirm_done',
+        cmp_autopairs.on_confirm_done()
+      )
+    end
   },
 }
