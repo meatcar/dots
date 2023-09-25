88566452cf0df0b7be138a5d6b8c15571cecc4af
Denys Pavlov <me@denys.me>
2023-09-24 21:42:20 -0400

    style(starship): config with a single attrset

--- home-manager/modules/starship/default.nix
+++ home-manager/modules/starship/default.nix
@@ -1,121 +1,126 @@
 { pkgs, ... }: {
-  programs.starship.enable = true;
-  # xdg.configFile."starship.toml".source = ./starship.toml;
-  programs.starship.settings = {
-    format = "$username$hostname$directory$fill $shell$all$line_break$shlvl$character";
-    right_format = "$cmd_duration$status";
-    time.disabled = false;
-    cmd_duration.show_notifications = true;
+  programs.starship = {
+    enable = true;
 
-    fill = {
-      symbol = "─";
-      style = "dimmed black";
+
+    settings = {
+      format = "$username$hostname$directory$fill $shell$all$line_break$shlvl$character";
+      right_format = "$cmd_duration$status";
+
+      time.disabled = false;
+      cmd_duration.show_notifications = true;
+
+      fill = {
+        symbol = "─";
+        style = "dimmed black";
+      };
+
+      hostname.style = "bold green";
+
+      username.format = "[$user]($style) ";
+      hostname.format = "in [$ssh_symbol $hostname]($style) ";
+      directory.format = "in [$path]($style)[$read_only]($read_only_style) ";
+
+      character = {
+        format = "$symbol";
+        success_symbol = "[\\$ ](bold green)"; #single-width
+        error_symbol = "[\\$ ](bold red)"; #single-width
+        vicmd_symbol = "[ ](bold green)"; #double-width
+      };
+
+      status = {
+        disabled = false;
+        format = "[$symbol$common_meaning$signal_name$maybe_int]($style) ";
+        success_symbol = "";
+        symbol = "  ";
+        not_executable_symbol = "  ";
+        not_found_symbol = "  ";
+        sigint_symbol = "  ";
+        signal_symbol = "!";
+        map_symbol = true;
+        pipestatus = true;
+        pipestatus_format = "\[$pipestatus\] => [$symbol$common_meaning$signal_name$maybe_int]($style)";
+      };
+
+      directory = {
+        truncation_symbol = "… /";
+        before_repo_root_style = "cyan";
+        repo_root_style = "bold cyan";
+        style = "cyan"; # after repo root
+        fish_style_pwd_dir_length = 1;
+      };
+
+      battery = {
+        full_symbol = "";
+        charging_symbol = "";
+        discharging_symbol = "";
+      };
+
+      git_status = {
+        staged = "+\${count}";
+        modified = "!\${count}";
+        ahead = "⇡\${count}";
+        diverged = "⇕⇡\${ahead_count}⇣\${behind_count}";
+        behind = "⇣\${count}";
+      };
+
+      shell = {
+        disabled = false;
+        fish_indicator = "fish";
+        bash_indicator = "bash";
+        zsh_indicator = "zsh";
+        powershell_indicator = "pwsh";
+      };
+      shlvl = {
+        disabled = false;
+        format = "[$symbol]($style)";
+        style = "dimmed black";
+        repeat = true;
+        symbol = "\\$";
+        repeat_offset = 1;
+        threshold = 0;
+      };
+
+      nix_shell = {
+        format = "via [$symbol$state( $name)]($style) ";
+        impure_msg = "";
+        pure_msg = "";
+      };
+
+
+      aws.symbol = " ";
+      buf.symbol = " ";
+      c.symbol = " ";
+      conda.symbol = "○ ";
+      dart.symbol = " ";
+      directory.read_only = "󰌾 ";
+      docker_context.symbol = " ";
+      elixir.symbol = " ";
+      elm.symbol = " ";
+      fossil_branch.symbol = " ";
+      git_branch.symbol = " ";
+      golang.symbol = " ";
+      guix_shell.symbol = " ";
+      haskell.symbol = " ";
+      haxe.symbol = " ";
+      hg_branch.symbol = " ";
+      hostname.ssh_symbol = "󰢹 ";
+      java.symbol = " ";
+      julia.symbol = " ";
+      lua.symbol = " ";
+      memory_usage.symbol = "󰍛 ";
+      meson.symbol = "󰔷 ";
+      nim.symbol = " ";
+      nix_shell.symbol = " ";
+      nodejs.symbol = "󰎙 ";
+      package.symbol = "󰏗 ";
+      pijul_channel.symbol = " ";
+      php.symbol = " ";
+      python.symbol = " ";
+      rlang.symbol = "󰟔 ";
+      ruby.symbol = " ";
+      rust.symbol = " ";
+      zig.symbol = "󱐋 ";
     };
-
-    hostname.style = "bold green";
-
-    username.format = "[$user]($style) ";
-    hostname.format = "in $ssh_symbol[$hostname]($style) ";
-    directory.format = "in [$path]($style)[$read_only]($read_only_style) ";
-
-    character = {
-      format = "$symbol";
-      success_symbol = "[\\$ ](bold green)"; #single-width
-      error_symbol = "[\\$ ](bold red)"; #single-width
-      vicmd_symbol = "[ ](bold green)"; #double-width
-    };
-
-    status = {
-      disabled = false;
-      format = "[$symbol$common_meaning$signal_name$maybe_int]($style) ";
-      success_symbol = "";
-      symbol = "  ";
-      not_executable_symbol = "  ";
-      not_found_symbol = "  ";
-      sigint_symbol = "  ";
-      signal_symbol = "!";
-      map_symbol = true;
-      pipestatus = true;
-      pipestatus_format = "\[$pipestatus\] => [$symbol$common_meaning$signal_name$maybe_int]($style)";
-    };
-
-    directory = {
-      truncation_symbol = "… /";
-      before_repo_root_style = "bold cyan";
-      repo_root_style = "bold green";
-      style = "green"; # after repo root
-    };
-
-    battery = {
-      full_symbol = "";
-      charging_symbol = "";
-      discharging_symbol = "";
-    };
-
-    git_status = {
-      staged = "+\${count}";
-      modified = "!\${count}";
-      ahead = "⇡\${count}";
-      diverged = "⇕⇡\${ahead_count}⇣\${behind_count}";
-      behind = "⇣\${count}";
-    };
-
-    shell = {
-      disabled = false;
-      fish_indicator = "fish";
-      bash_indicator = "bash";
-      zsh_indicator = "zsh";
-      powershell_indicator = "pwsh";
-    };
-    shlvl = {
-      disabled = false;
-      format = "[$symbol]($style)";
-      style = "dimmed black";
-      repeat = true;
-      symbol = "\\$";
-      repeat_offset = 1;
-      threshold = 0;
-    };
-
-    nix_shell = {
-      format = "via [$symbol$state( $name)]($style) ";
-      impure_msg = "";
-      pure_msg = "";
-    };
-
-
-    aws.symbol = " ";
-    buf.symbol = " ";
-    c.symbol = " ";
-    conda.symbol = "○ ";
-    dart.symbol = " ";
-    directory.read_only = "󰌾 ";
-    docker_context.symbol = " ";
-    elixir.symbol = " ";
-    elm.symbol = " ";
-    fossil_branch.symbol = " ";
-    git_branch.symbol = " ";
-    golang.symbol = " ";
-    guix_shell.symbol = " ";
-    haskell.symbol = " ";
-    haxe.symbol = " ";
-    hg_branch.symbol = " ";
-    hostname.ssh_symbol = "󰢹 ";
-    java.symbol = " ";
-    julia.symbol = " ";
-    lua.symbol = " ";
-    memory_usage.symbol = "󰍛 ";
-    meson.symbol = "󰔷 ";
-    nim.symbol = " ";
-    nix_shell.symbol = " ";
-    nodejs.symbol = "󰎙 ";
-    package.symbol = "󰏗 ";
-    pijul_channel.symbol = " ";
-    php.symbol = " ";
-    python.symbol = " ";
-    rlang.symbol = "󰟔 ";
-    ruby.symbol = " ";
-    rust.symbol = " ";
-    zig.symbol = "󱐋 ";
   };
 }
