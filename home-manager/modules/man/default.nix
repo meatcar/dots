bc631f05d89d3b3950a6a5a32592928d3de8f0f8
Denys Pavlov <me@denys.me>
2023-09-26 19:39:24 -0400

    feat(home-manager): colorize manpages the best way(tm)

--- home-manager/modules/man/default.nix
+++ home-manager/modules/man/default.nix
@@ -1,7 +1,32 @@
-{ config, ... }:
+{ config, pkgs, lib, ... }:
 let
-  inherit (builtins) getEnv;
   user = config.home.username;
+  man_color_args = lib.strings.concatStringsSep " " [
+    "-DP+k-" # Prompt = formatted bold Black on normal
+    "-DE+kr" # Error/Info = formatted bold Black on Red
+    "-DS+ky" # Search result = formatted bold Black on Yellow
+    "-Dd+c" # bold = formatted cyan
+    "-Dk+c" # blink = formatted cyan
+    "-Du+g" # underline = formatted green
+    "-Ds+kw" # standout = formatted Black on White
+  ];
+  manpager = pkgs.writeShellScriptBin "manpager" ''
+    export LESS_TERMCAP_mb=$(tput bold)             # begin blinking
+    export LESS_TERMCAP_md=$(tput bold)             # begin bold
+    export LESS_TERMCAP_me=$(tput sgr0)             # end mode
+    export LESS_TERMCAP_so=$(tput smso; tput bold)  # begin standout moude
+    export LESS_TERMCAP_se=$(tput rmso; tput sgr0)  # end standout moude
+    export LESS_TERMCAP_us=$(tput bold; tput smul)  # begin underline
+    export LESS_TERMCAP_ue=$(tput rmul; tput sgr0)  # end underline 
+    export LESS_TERMCAP_mr=$(tput rev)              # reverse
+    export LESS_TERMCAP_mh=$(tput dim)              # half-bright
+    export LESS_TERMCAP_ZN=$(tput ssubm)
+    export LESS_TERMCAP_ZV=$(tput rsubm)
+    export LESS_TERMCAP_ZO=$(tput ssupm)
+    export LESS_TERMCAP_ZW=$(tput rsupm)
+
+    less --use-color ${man_color_args} "$@"
+  '';
 in
 {
   programs.man.enable = true;
@@ -13,5 +38,10 @@ in
       MANDB_MAP   /home/${user}/.nix-profile/share/man ${config.xdg.cacheHome}/man
     '';
     onChange = "mkdir -p $XDG_CACHE_HOME/man && mandb -u";
+
+  home.sessionVariables = {
+    MANROFFOPT = "-c";
+    MANPAGER = "manpager";
   };
+  home.packages = [ manpager ];
 }
--- home-manager/systems/common.nix
+++ home-manager/systems/common.nix
@@ -64,23 +64,7 @@
   home.sessionVariables = {
     EDITOR = "nvim";
     NOTES_DIR = "~/Sync/notes";
-    # color man
-    MANPAGER = "less";
     LESS = "-R --mouse";
-    LESS_TERMCAP_mb = "$(tput bold; tput setaf 2)";
-    LESS_TERMCAP_md = "$(tput bold; tput setaf 6)";
-    LESS_TERMCAP_me = "$(tput sgr0)";
-    LESS_TERMCAP_so = "$(tput bold)";
-    LESS_TERMCAP_se = "$(tput rmso; tput sgr0)";
-    LESS_TERMCAP_us = "$(tput bold; tput setaf 4)";
-    LESS_TERMCAP_ue = "$(tput sgr0)";
-    LESS_TERMCAP_mr = "$(tput rev)";
-    LESS_TERMCAP_mh = "$(tput dim)";
-    LESS_TERMCAP_ZN = "$(tput ssubm)";
-    LESS_TERMCAP_ZV = "$(tput rsubm)";
-    LESS_TERMCAP_ZO = "$(tput ssupm)";
-    LESS_TERMCAP_ZW = "$(tput rsupm)";
-
   };
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
