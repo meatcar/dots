097b2a7a3edb7fadbf53e73e1beb26ad8bee29b1
Denys Pavlov <me@denys.me>
2023-10-04 01:41:49 -0400

    fix(nix): don't build zellij

--- home-manager/modules/zellij/default.nix
+++ home-manager/modules/zellij/default.nix
@@ -8,6 +8,6 @@
     };
     # use third-party zellij package that provides plugins until issue is fixed:
     # https://github.com/NixOS/nixpkgs/issues/197377
-    package = specialArgs.inputs.zellij.packages."${pkgs.system}".default;
+    # package = specialArgs.inputs.zellij.packages."${pkgs.system}".default;
   };
 }
