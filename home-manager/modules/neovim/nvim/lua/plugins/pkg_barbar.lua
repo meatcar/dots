da37bcee7e3f0b3bba223ce56471c4079110fb6f
Denys Pavlov <me@denys.me>
2023-11-06 14:37:53 -0500

    feat(nvim): fix up tab bar styling

--- home-manager/modules/neovim/nvim/lua/plugins/pkg_barbar.lua
+++ home-manager/modules/neovim/nvim/lua/plugins/pkg_barbar.lua
@@ -12,18 +12,29 @@ return {
     end
     require('bufferline').setup {
       animation = false,
-      auto_hide = true,
       exclude_ft = _G.me.o.panels,
       sidebar_filetypes = sidebars,
+      auto_hide = 1,
+      -- Uncomment to only show pinned tabs:
+      -- auto_hide = 0,
+      -- hide = {
+      --   current = true,
+      --   inactive = true,
+      --   visible = true,
+      -- },
       icons = {
         button = '',
-        modified = { button = '' },
-        pinned = { button = '車' },
+        modified = { button = ' ' },
+        pinned = { button = ' ', filename = true },
+        separator_at_end = false,
+        inactive = { separator = { left = ' ' } },
       },
       diagnostic = {
         [vim.diagnostic.severity.ERROR] = { enabled = true },
       },
       highlight_inactive_file_icons = true,
+      maximum_padding = 4,
+      minimum_padding = 1,
     }
     require('core/keymaps').barbar()
   end,
