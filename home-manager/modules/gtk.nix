474c8d5f78f233f45bbffe7a58c640be8d5669ce
Denys Pavlov <me@denys.me>
2023-12-16 00:14:46 -0500

    feat(gtk): use the default adwaita theme

--- home-manager/modules/gtk.nix
+++ home-manager/modules/gtk.nix
@@ -1,13 +1,7 @@
 { pkgs, ... }:
 {
-  home.packages = [ pkgs.dconf ];
+  home.packages = with pkgs; [dconf glib.bin];
   gtk = {
     enable = true;
-    iconTheme.package = pkgs.papirus-icon-theme;
-    iconTheme.name = "Papirus";
-    theme.package = pkgs.pop-gtk-theme;
-    theme.name = "Pop-dark";
-    font.package = pkgs.inter;
-    font.name = "Inter 10";
   };
 }
