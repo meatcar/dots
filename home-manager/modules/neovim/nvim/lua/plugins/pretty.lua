e6426f86fc79e87f47ddda2d28ed2172d71c1049
Denys Pavlov <me@denys.me>
2024-02-28 14:35:08 -0500

    fix(nvim): disable specs.nvim due to errors

--- home-manager/modules/neovim/nvim/lua/plugins/pretty.lua
+++ home-manager/modules/neovim/nvim/lua/plugins/pretty.lua
@@ -65,29 +65,30 @@ return {
     },
   },
 
-  { -- flash cursor sometimes
-    'edluffy/specs.nvim',
-    event = me.o.events.verylazy,
-    config = function()
-      require('specs').setup {
-        show_jumps = true,
-        min_jump = 30,
-        popup = {
-          delay_ms = 0, -- delay before popup displays
-          inc_ms = 10,  -- time increments used for fade/resize effects
-          blend = 10,   -- starting blend, between 0-100 (fully transparent), see :h winblend
-          width = 10,
-          winhl = 'PMenu',
-          fader = require('specs').linear_fader,
-          resizer = require('specs').shrink_resizer,
-        },
-        ignore_filetypes = {},
-        ignore_buftypes = {
-          nofile = true,
-        },
-      }
-    end,
-  },
+  -- TODO: lots of errors. check for update and reactivate
+  -- { -- flash cursor sometimes
+  --   'edluffy/specs.nvim',
+  --   event = me.o.events.verylazy,
+  --   config = function()
+  --     require('specs').setup {
+  --       show_jumps = true,
+  --       min_jump = 30,
+  --       popup = {
+  --         delay_ms = 0, -- delay before popup displays
+  --         inc_ms = 10,  -- time increments used for fade/resize effects
+  --         blend = 10,   -- starting blend, between 0-100 (fully transparent), see :h winblend
+  --         width = 10,
+  --         winhl = 'PMenu',
+  --         fader = require('specs').linear_fader,
+  --         resizer = require('specs').shrink_resizer,
+  --       },
+  --       ignore_filetypes = {},
+  --       ignore_buftypes = {
+  --         nofile = true,
+  --       },
+  --     }
+  --   end,
+  -- },
 
   { -- highlight word under cursor
     'RRethy/vim-illuminate',
