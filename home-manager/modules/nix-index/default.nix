e3f8ca885efe08ed7afd45e6d1fed09aa3989d80
Denys Pavlov <me@denys.me>
2024-12-26 00:17:59 -0500

    chore: nix flake update

--- home-manager/modules/nix-index/default.nix
+++ home-manager/modules/nix-index/default.nix
@@ -7,5 +7,6 @@
     specialArgs.inputs.nix-index-database.hmModules.nix-index
   ];
   programs.nix-index.enable = true;
-  programs.nix-index-database.comma.enable = true;
+  # TODO: fixes https://github.com/nix-community/nix-index-database/issues/134
+  # programs.nix-index-database.comma.enable = true;
 }
