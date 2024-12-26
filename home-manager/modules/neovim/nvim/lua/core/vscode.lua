829b602e2b6a0fd924384ca2c9a6be7c8a1818df
Denys Pavlov <me@denys.me>
2024-12-25 23:59:20 -0500

    feat(nvim): use vscodespace

--- home-manager/modules/neovim/nvim/lua/core/vscode.lua
+++ home-manager/modules/neovim/nvim/lua/core/vscode.lua
@@ -1,6 +1,9 @@
+-- Sync vim yanking with system clipboard
+vim.o.clipboard = "unnamedplus"
+
 vim.keymap.set('x', 'gc', '<Plug>VSCodeCommentary', {})
 vim.keymap.set('n', 'gc', '<Plug>VSCodeCommentary', {})
 vim.keymap.set('o', 'gc', '<Plug>VSCodeCommentary', {})
 vim.keymap.set('n', 'gcc', '<Plug>VSCodeCommentaryLine', {})
-vim.keymap.set('n', '<Space>/', function() vscode.call('workbench.action.quickTextSearch') end, {})
-vim.keymap.set('n', '<Space>bd', function() vscode.call('workbench.action.closeActiveEditor') end, {})
+vim.keymap.set('n', '<Space>', function() require('vscode').action('vspacecode.space') end)
+vim.keymap.set('x', '<Space>', function() require('vscode').action('vspacecode.space') end, { remap = true })
