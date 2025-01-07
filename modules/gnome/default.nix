2bb5846ac5d689e9a1241b69ddecacfaf706a004
Denys Pavlov <me@denys.me>
2025-01-06 23:49:44 -0500

    feat(nix): watson config

---
+++ modules/gnome/default.nix
@@ -0,0 +1,31 @@
+{pkgs, ...}: {
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  environment.gnome.excludePackages = with pkgs; [
+    gnome-photos
+    gnome-tour
+    gnome-music
+    gedit # text editor
+    epiphany # web browser
+    geary # email reader
+    gnome-characters
+    tali # poker game
+    iagno # go game
+    hitori # sudoku game
+    atomix # puzzle game
+    yelp # Help view
+    gnome-contacts
+    gnome-initial-setup
+  ];
+
+  environment.systemPackages = with pkgs; [
+    gnome-tweaks
+
+    # for panel
+    iw
+    iotop
+    amdgpu_top
+    gtop
+    nethogs
+  ];
+}
