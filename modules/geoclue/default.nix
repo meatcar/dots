09abdbdcbc1602a762c093a905fa615ad4192c1b
Denys Pavlov <me@denys.me>
2025-01-06 23:49:44 -0500

    feat(nix): watson config

---
+++ modules/geoclue/default.nix
@@ -0,0 +1,13 @@
+{
+  location.provider = "geoclue2";
+  services.geoclue2 = {
+    enable = true;
+    geoProviderUrl = "https://api.beacondb.net/v1/geolocate";
+    appConfig = {
+      "nl.whynothugo.darkman" = {
+        isAllowed = true;
+        isSystem = false;
+      };
+    };
+  };
+}
--- systems/watson/default.nix
+++ systems/watson/default.nix
@@ -1,10 +1,67 @@
-{...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
   imports = [
     ./disko.nix
-    ../../modules/impermanence
-    ../common.nix
-    ./configuration.nix
     ./hardware-configuration.nix
+    ../common.nix
+    ../../modules/impermanence
+    ../../modules/geoclue
+    ../../modules/gnome
   ];
   system.stateVersion = "24.11";
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot";
+  networking.hostName = "watson"; # Define your hostname.
+  networking.networkmanager.enable = true;
+  services.printing.enable = true;
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    neovim
+    wget
+
+    efibootmgr
+    radeontop
+    v4l-utils
+  ];
+
+  services.fwupd.enable = true;
+  systemd.timers.fwupd-refresh.enable = false; # https://github.com/NixOS/nixpkgs/issues/271834
+
+  programs._1password.enable = true;
+  programs._1password-gui.enable = true;
+  programs._1password-gui.polkitPolicyOwners = ["meatcar"];
+
+  # needs tuning. maybe use tlp?
+  # powerManagement.powertop.enable = true;
+  services.power-profiles-daemon.enable = false;
+  services.auto-cpufreq.enable = true;
+  services.auto-cpufreq.settings = {
+    battery = {
+      governor = "powersave";
+      turbo = "never";
+    };
+    charger = {
+      governor = "performance";
+      turbo = "auto";
+    };
+  };
+
+  nix.settings.trusted-users = ["meatcar"];
+  users.mutableUsers = false;
+  users.users.meatcar = {
+    isNormalUser = true;
+    useDefaultShell = false;
+    shell = "${pkgs.fish}/bin/fish";
+    # nix-shell -p mkpasswd --command 'mkpasswd -m sha-512'
+    hashedPassword = "$6$d60LJzot5J$PeWx9sU6rPNEy39uSewpJiV5CfOh9McENT5Crl4WCFyvwL/5jyH7Jn2pENG6pEWPNNFl2Xnp4WGEJEMAU2Mym0";
+    extraGroups = ["wheel"];
+  };
 }
