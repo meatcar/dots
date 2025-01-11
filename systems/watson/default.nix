d3151d5f9beb1d8a9ca6e035373360044a11c0df
Denys Pavlov <me@denys.me>
2025-01-11 11:12:14 -0500

    feat(t14s): +docker

--- systems/watson/default.nix
+++ systems/watson/default.nix
@@ -13,6 +13,7 @@
     ../../modules/power
     ../../modules/geoclue
     ../../modules/gnome
+    ../../modules/docker.nix
   ];
   system.stateVersion = "24.11";
 
@@ -90,6 +91,6 @@
     shell = "${pkgs.fish}/bin/fish";
     # nix-shell -p mkpasswd --command 'mkpasswd -m sha-512'
     hashedPassword = "***REDACTED***";
-    extraGroups = ["wheel"];
+    extraGroups = ["wheel" "docker"];
   };
 }
