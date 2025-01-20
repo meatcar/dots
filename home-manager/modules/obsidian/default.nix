8fccd2dcf00a39ca98ae5c9890c9710337b116f8
Denys Pavlov <me@denys.me>
2025-01-20 13:32:34 -0500

    feat(nix): +obsidian

---
+++ home-manager/modules/obsidian/default.nix
@@ -0,0 +1,6 @@
+{pkgs, ...}: {
+  nixpkgs.config.allowUnfree = true;
+  home.packages = with pkgs; [
+    obsidian
+  ];
+}
--- home-manager/systems/watson/default.nix
+++ home-manager/systems/watson/default.nix
@@ -14,6 +14,7 @@
     ../../modules/docker
     ../../modules/activitywatch
     ../../modules/ghostty
+    ../../modules/obsidian
     ./impermanence.nix
   ];
 
