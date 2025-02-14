7e5d378576a22c6700c8eafcd816445b68913e7d
Denys Pavlov <me@denys.me>
2025-02-13 22:06:42 -0500

    feat(watson): udev key mappings with keyd

---
+++ modules/keyd/default.nix
@@ -0,0 +1,29 @@
+{...}: {
+  services.keyd = {
+    enable = true;
+    keyboards = {
+      default = {
+        ids = ["*"];
+        settings = {
+          main = {
+            capslock = "esc";
+            "rightcontrol" = "layer(rightcontrol)";
+            "wakeup" = "layer(rightcontrol)"; # t14s fn = "wakeup"
+          };
+          rightcontrol = {
+            # mac-ish bindings
+            "left" = "home";
+            "right" = "end";
+            "up" = "pageup";
+            "down" = "pagedown";
+
+            # makes sence on a leopold fc660c
+            "insert" = "volumeup";
+            "delete" = "volumedown";
+            "backspace" = "mute";
+          };
+        };
+      };
+    };
+  };
+}
