52d4da1d5ec3145bf14912767708132c6e6a8bbb
Denys Pavlov <me@denys.me>
2025-03-13 13:12:49 -0400

    chore: nix flake update

--- flake.nix
+++ flake.nix
@@ -6,12 +6,13 @@
       "https://cache.nixos.org"
       "https://nix-community.cachix.org"
       "https://nixpkgs-wayland.cachix.org"
-      "https://wurzelpfropf.cachix.org"
+      "https://wurzelpfropf.cachix.org" # ragenix
     ];
     extra-trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+      "wurzelpfropf.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
     ];
   };
 
