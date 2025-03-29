5ac6f8e1b3b29d18769a29dc8c97d9f341521eb5
Denys Pavlov <me@denys.me>
2025-03-29 13:30:33 -0400

    fix(activitywatch): keep fighting awatcher not auto-starting

--- home-manager/modules/activitywatch/default.nix
+++ home-manager/modules/activitywatch/default.nix
@@ -1,14 +1,35 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   services.activitywatch = {
     enable = true;
     package = pkgs.aw-server-rust;
     watchers = {
       awatcher = {
         package = pkgs.awatcher;
-      };
-      aw-watcher-afk = {
-        package = pkgs.aw-server-rust;
+        settings = {
+          idle-timeout-seconds = 180;
+          poll-time-idle-seconds = 10;
+          poll-time-window-seconds = 5;
+        };
       };
     };
+  };
+  systemd.user.services.activitywatch-watcher-awatcher = {
+    Unit.Requires = ["activitywatch.service"];
+    Service = {
+      # delay start for activiywatch to spin up.
+      ExecStartPre = "/run/current-system/sw/bin/sleep 5";
+      # make fault-tolerant
+      Restart = "always";
+    };
+  };
+
+  # FIXME: workaround for https://github.com/nix-community/home-manager/issues/5988
+  systemd.user.targets.activitywatch = {
+    Unit.After = lib.mkForce ["graphical-session.target"];
+    Install.WantedBy = lib.mkForce ["graphical-session.target"];
   };
 }
