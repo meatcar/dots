f384a323b152037ec3aa4bce1875a492b831c061
Denys Pavlov <me@denys.me>
2025-03-29 13:29:49 -0400

    feat(vscode): gitignore misc ai tool artifacts

--- home-manager/modules/vscode/default.nix
+++ home-manager/modules/vscode/default.nix
@@ -7,4 +7,9 @@
     target = ".ssh/config_source";
     onChange = ''cat ~/.ssh/config_source > ~/.ssh/config && chmod 400 ~/.ssh/config'';
   };
+  programs.git.ignores = [
+    ".roo*"
+    ".repomixignore"
+    "memory-bank/"
+  ];
 }
