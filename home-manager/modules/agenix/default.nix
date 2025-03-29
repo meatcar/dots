7f2dd4d6afd7e73731a5889d314734cc8ea90439
Denys Pavlov <me@denys.me>
2025-03-29 13:18:28 -0400

    fix(secrets): home-manager runtime dir fix

--- home-manager/modules/agenix/default.nix
+++ home-manager/modules/agenix/default.nix
@@ -1,3 +1,7 @@
-{
+let
+  # FIXME: https://github.com/ryantm/agenix/issues/300
+  xdgRuntimeDir = "/run/user/1000";
+in {
   imports = [../../../secrets/module.nix];
+  age.secretsDir = "${xdgRuntimeDir}/agenix";
 }
