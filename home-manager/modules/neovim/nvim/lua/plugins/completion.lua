3e0d4019367ae023644e06378d419c3a4afbbb2a
Denys Pavlov <me@denys.me>
2025-04-11 22:36:04 -0400

    feat(nvim): use blink over nvim-cmp
    - update autopair config
    - update lspconfigs

--- home-manager/modules/neovim/nvim/lua/plugins/completion.lua
+++ home-manager/modules/neovim/nvim/lua/plugins/completion.lua
@@ -1,191 +1,117 @@
+-- TODO: delete. Keeping for reference.
+-- {
+--   'hrsh7th/nvim-cmp',
+--   dependencies = {
+--     'hrsh7th/cmp-nvim-lsp',
+--     'hrsh7th/cmp-buffer',
+--     'hrsh7th/cmp-omni',
+--     'FelipeLema/cmp-async-path',
+--     -- 'hrsh7th/cmp-cmdline',
+--     'hrsh7th/cmp-nvim-lsp-signature-help',
+--     'kristijanhusak/vim-dadbod-completion',
+--     'lukas-reineke/cmp-under-comparator',
+--     'lukas-reineke/cmp-rg',
+--     'andersevenrud/cmp-tmux', -- Sources words from adjacent tmux panes.
+--     'onsails/lspkind-nvim',   -- add icons to lsp completions
+--     { 'saadparwaiz1/cmp_luasnip' },
+--     { 'doxnit/cmp-luasnip-choice', opts = {} },
+--   },
+
 return {
   {
-    'hrsh7th/nvim-cmp',
+    'saghen/blink.cmp',
+    -- optional: provides snippets for the snippet source
     dependencies = {
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-buffer',
-      'hrsh7th/cmp-omni',
-      'FelipeLema/cmp-async-path',
-      -- 'hrsh7th/cmp-cmdline',
-      'hrsh7th/cmp-nvim-lsp-signature-help',
-      'kristijanhusak/vim-dadbod-completion',
-      'lukas-reineke/cmp-under-comparator',
-      'lukas-reineke/cmp-rg',
-      'andersevenrud/cmp-tmux', -- Sources words from adjacent tmux panes.
-      'onsails/lspkind-nvim',   -- add icons to lsp completions
-      { 'saadparwaiz1/cmp_luasnip' },
-      { 'doxnit/cmp-luasnip-choice', opts = {} },
+      'rafamadriz/friendly-snippets',
+      'mgalliou/blink-cmp-tmux',
+      'disrupted/blink-cmp-conventional-commits',
     },
+
     event = me.o.events.insert,
-    config = function()
-      vim.cmd [[
-        autocmd me FileType sql,mysql,plsql lua require('cmp').setup.buffer({ sources = {{ name = 'vim-dadbod-completion' }} })
-      ]]
+    -- use a release tag to download pre-built binaries
+    version = '1.*',
+    -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+    -- build = 'cargo build --release',
+    -- If you use nix, you can build from source using latest nightly rust with:
+    -- build = 'nix run .#build-plugin',
 
-      local cmp = require 'cmp'
-      local luasnip = require("luasnip")
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+      -- 'super-tab' for mappings similar to vscode (tab to accept)
+      -- 'enter' for enter to accept
+      -- 'none' for no mappings
+      --
+      -- All presets have the following mappings:
+      -- C-space: Open menu or open docs if already open
+      -- C-n/C-p or Up/Down: Select next/previous item
+      -- C-e: Hide menu
+      -- C-k: Toggle signature help (if signature.enabled = true)
+      --
+      appearance = {
+        -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        -- Adjusts spacing to ensure icons are aligned
+        nerd_font_variant = 'mono'
+      },
 
-      local has_words_before = function()
-        unpack = unpack or table.unpack
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-      end
+      completion = {
+        -- (Default) Only show the documentation popup when manually triggered
+        documentation = { auto_show = true, auto_show_delay_ms = 250 },
+        -- autopair completion
+        accept = { auto_brackets = { enabled = true } }
+      },
 
-      ---@diagnostic disable-next-line: missing-fields
-      cmp.setup {
-        ---@diagnostic disable-next-line: missing-fields
-        performance = {
-          debounce = 500,
-          throttle = 500,
-        },
-        sources = cmp.config.sources({
-          { name = 'nvim_lsp' },
-          { name = 'nvim_lsp_signature_help' },
-          {
-            name = 'luasnip',
-            entry_filter = function()
-              local context = require("cmp.config.context")
-              return not context.in_treesitter_capture("string") and not context.in_syntax_group("String")
-            end,
-          },
-          { name = 'luasnip_choice' }
-        }, {
-          { name = 'async_path' },
-        }, {
-          { name = 'buffer' },
-          { name = 'rg' },
-          {
+      -- Default list of enabled providers defined so that you can extend it
+      -- elsewhere in your config, without redefining it, due to `opts_extend`
+      sources = {
+        default = { 'lsp', 'path', 'snippets', 'buffer', 'tmux' },
+        providers = {
+          tmux = {
+            module = 'blink-cmp-tmux',
             name = 'tmux',
-            option = {
-              all_panes = false,
+            opts = {
+              all_panes = true
             }
           },
-        }),
-        mapping = cmp.mapping.preset.insert {
-          ['<C-x><C-o>'] = cmp.mapping.complete(),
-          ---@diagnostic disable-next-line: missing-fields
-          ['<C-x><C-f>'] = cmp.mapping.complete({ config = { sources = { { name = 'async_path' } } } }),
-          ---@diagnostic disable-next-line: missing-fields
-          ['<C-x><C-s>'] = cmp.mapping.complete({ config = { sources = { { name = 'luasnip' } } } }),
-          ---@diagnostic disable-next-line: missing-fields
-          ['<C-x><C-t>'] = cmp.mapping.complete({ config = { sources = { { name = 'tmux' } } } }),
-          ---@diagnostic disable-next-line: missing-fields
-          ['<C-x><C-r>'] = cmp.mapping.complete({ config = { sources = { { name = 'rg' } } } }),
-          ['<C-Space>'] = cmp.mapping.confirm { select = true },
-          ['<C-g>'] = cmp.mapping.abort(),
-          ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            -- local copilot = require("copilot.suggestion")
-            -- if copilot.is_visible() then
-            --   copilot.accept()
-            -- else
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            elseif has_words_before() then
-              cmp.complete()
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-
-          ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-          ["<CR>"] = cmp.mapping({
-            i = function(fallback)
-              if cmp.visible() and cmp.get_active_entry() then
-                cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
-              else
-                fallback()
-              end
+          conventional_commits = {
+            name = 'Conventional Commits',
+            module = 'blink-cmp-conventional-commits',
+            enabled = function()
+              return vim.bo.filetype == 'gitcommit'
             end,
-            s = cmp.mapping.confirm({ select = true }),
-            c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
-          }),
-        },
-        snippet = {
-          expand = function(args) -- REQUIRED - you must specify a snippet engine
-            require 'luasnip'.lsp_expand(args.body)
-          end,
-        },
-        ---@diagnostic disable-next-line: missing-fields
-        sorting = {
-          comparators = {
-            cmp.config.compare.offset,
-            cmp.config.compare.exact,
-            cmp.config.compare.score,
-            require('cmp-under-comparator').under,
-            cmp.config.compare.kind,
-            cmp.config.compare.sort_text,
-            cmp.config.compare.length,
-            cmp.config.compare.order,
+            ---@module 'blink-cmp-conventional-commits'
+            ---@type blink-cmp-conventional-commits.Options
+            opts = {}, -- none so far
           },
         },
-        ---@diagnostic disable-next-line: missing-fields
-        view = {
-          entries = {
-            name = 'custom',
-            selection_order = 'near_cursor',
-          },
-        },
-        -- experimental = { ghost_text = false },
-        window = {
-          ---@diagnostic disable-next-line: missing-fields
-          completion = {
-            winhighlight = 'Normal:Pmenu,FloatBorder:Pmenu,Search:None',
-            col_offset = -1,
-            side_padding = 0,
-          },
-        },
-        ---@diagnostic disable-next-line: missing-fields
-        formatting = {
-          fields = { 'menu', 'abbr', 'kind' },
-          format = function(entry, vim_item)
-            local cmp_format = require('lspkind').cmp_format {
-              mode = 'symbol_text',
-              maxwidth = 50,
-              menu = {
-                path = ' ',
-                async_path = ' ',
-                rg = '󰍉 ',
-                tmux = ' ',
-                buffer = '󰧭 ',
-                nvim_lsp = '󰘧 ',
-                vsnip = '󰢵 ',
-                luasnip = '󰢵 ',
-                luasnip_choice = '󰢵 ',
-                ['vim-dadbod-completion'] = '󱘲 ',
-              },
-              symbol_map = {
-                TypeParameter = ' ',
-              },
-            }
-            local kind = cmp_format(entry, vim_item)
-            local menu = kind.menu
-            local strings = vim.split(kind.kind, '%s', { trimempty = true })
-            if #strings >= 2 then
-              kind.kind = ('%s  %s'):format(strings[1], strings[2])
-              kind.menu = ('%s'):format(menu)
+        per_filetype = {
+          codecompanion = { "codecompanion" },
+        }
+      },
+
+      -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+      -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+      -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+      --
+      -- See the fuzzy documentation for more information
+      fuzzy = { implementation = "prefer_rust_with_warning" },
+
+      -- See :h blink-cmp-config-keymap for defining your own keymap
+      keymap = {
+        preset = 'enter',
+        ['<Tab>'] = {
+          function(cmp)
+            local copilot = require("copilot.suggestion")
+            if copilot.is_visible() then
+              copilot.accept()
+              return true
             end
-            return kind
           end,
-        },
+          'select_next'
+        }
       }
-
-      ---@diagnostic disable-next-line: missing-fields
-      cmp.setup.filetype("DressingInput", {
-        sources = cmp.config.sources { { name = "omni" } },
-      })
-    end,
+    },
+    opts_extend = { "sources.default" }
   },
-
 }
--- home-manager/modules/neovim/nvim/lua/plugins/lsp.lua
+++ home-manager/modules/neovim/nvim/lua/plugins/lsp.lua
@@ -29,85 +29,82 @@ return {
     config = function()
       require 'neodev'
       require 'cmp'
-      local default_capabilities = vim.lsp.protocol.make_client_capabilities()
-      local capabilities = require('cmp_nvim_lsp').default_capabilities(default_capabilities)
+      local lspconfig = require 'lspconfig'
+      local servers = {
+        lua_ls = {
+          settings = {
+            Lua = {
+              -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+              runtime = { version = 'LuaJIT' },
+              workspace = {
+                checkThirdParty = false, -- disable unhelpful prompts
+              },
+              -- Do not send telemetry data containing a randomized but unique identifier
+              telemetry = { enable = false },
+            },
+          },
+        },
+        nil_ls = {
+          formatting = {
+            command = 'alejandra',
+          }
+        },
+        terraformls = {},
+        gopls = {},
+        ansiblels = {},
+        elixirls = {},
+        eslint = {},
+        cssls = {
+          handlers = {
+            ["textDocument/diagnostic"] = function() end
+          }
+        },
+        html = {},
+        bashls = {},
+        dockerls = {},
+        zls = {},
+        ts_ls = {
+          root_dir = lspconfig.util.root_pattern("package.json"),
+          single_file_support = false
+        },
+        jsonls = {
+          settings = {
+            json = {
+              schemas = require('schemastore').json.schemas(),
+              validate = { enable = true },
+            },
+          },
+          handlers = {
+            ["textDocument/diagnostic"] = function() end
+          }
+        },
+        yamlls = {
+          settings = {
+            yaml = {
+              schemas = require('schemastore').yaml.schemas(),
+              schemaStore = { enable = false, url = "" }, -- disable built-in schemaStore support
+            },
+          },
+        }
+      }
 
-      local on_attach = function(client, bufnr)
-        require('illuminate').on_attach(client)
-        keymaps.lsp_on_attach(client, bufnr)
+      local default_capabilities = vim.lsp.protocol.make_client_capabilities()
+      local capabilities = vim.tbl_deep_extend('force', default_capabilities,
+        require('blink.cmp').get_lsp_capabilities({}, false))
+      local on_attach = keymaps.lsp_on_attach
+
+      for server, config in pairs(servers) do
+        config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
+        config.on_attach = on_attach
+        lspconfig[server].setup(config)
       end
 
-      local lspconfig = require 'lspconfig'
-      local util = require 'lspconfig.util'
-      util.default_config = vim.tbl_extend('force', util.default_config, {
-        capabilities = capabilities,
-        on_attach = on_attach,
-      })
-
-      lspconfig.lua_ls.setup {
-        settings = {
-          Lua = {
-            -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-            runtime = { version = 'LuaJIT' },
-            workspace = {
-              checkThirdParty = false, -- disable unhelpful prompts
-            },
-            -- Do not send telemetry data containing a randomized but unique identifier
-            telemetry = { enable = false },
-          },
-        },
-      }
-      lspconfig.nil_ls.setup {
-        formatting = {
-          command = 'alejandra',
-        }
-      }
-      lspconfig.terraformls.setup {}
-      lspconfig.gopls.setup {}
-      lspconfig.ansiblels.setup {}
-      lspconfig.elixirls.setup {}
-      lspconfig.eslint.setup {}
-      capabilities.textDocument.completion.completionItem.snippetSupport = true
-      lspconfig.cssls.setup {
-        capabilities = capabilities,
-        handlers = {
-          ["textDocument/diagnostic"] = function() end
-        }
-      }
-      lspconfig.html.setup {}
-      lspconfig.bashls.setup {}
-      lspconfig.dockerls.setup {}
-      lspconfig.zls.setup {}
-      lspconfig.jsonls.setup {
-        settings = {
-          json = {
-            schemas = require('schemastore').json.schemas(),
-            validate = { enable = true },
-          },
-        },
-        handlers = {
-          ["textDocument/diagnostic"] = function() end
-        }
-      }
-      lspconfig.yamlls.setup {
-        settings = {
-          yaml = {
-            schemas = require('schemastore').yaml.schemas(),
-            schemaStore = { enable = false, url = "" }, -- disable built-in schemaStore support
-          },
-        },
-      }
       require("deno-nvim").setup {
         server = {
           root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
           capabilities = capabilities,
           on_attach = on_attach,
         }
-      }
-
-      lspconfig.ts_ls.setup {
-        root_dir = lspconfig.util.root_pattern("package.json"),
-        single_file_support = false
       }
     end,
   },
