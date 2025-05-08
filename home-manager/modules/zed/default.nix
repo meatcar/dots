3833c71f5a451e067103f71cbe4e8baa6eedec58
Denys Pavlov <me@denys.me>
2025-05-08 15:45:56 -0400

    fix(zed): alias zed to zeditor

--- home-manager/modules/zed/default.nix
+++ home-manager/modules/zed/default.nix
@@ -1,3 +1,9 @@
-{
+{pkgs, ...}: {
   programs.zed-editor.enable = true;
+  # some programs assume zeditor is zed
+  home.packages = [
+    (pkgs.writeShellScriptBin "zed" ''
+      zeditor "$@"
+    '')
+  ];
 }
