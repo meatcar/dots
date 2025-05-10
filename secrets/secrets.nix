77b6177ac19031ad1545865305ae52abded17299
Denys Pavlov <me@denys.me>
2025-05-10 08:44:30 -0400

    ci: rely on secret ssh key to decrypt secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,9 +1,11 @@
 let
   mormont = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBcq01gh2tn/+hcm75N3LnS003mUBjXcT6qNndMhObPO";
   watson = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOlUHaqIExPCm99pWSUhsi2O2ic3KwRJ/VUmJ28b2xNM meatcar@watson";
+  ci = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHtmRcvR5Ron/AnnOhksodO+8ZSYTfM3RtURqnQWTrTw meatcar@ci";
   systems = [
     mormont
     watson
+    ci
   ];
 in
 {
