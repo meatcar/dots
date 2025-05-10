47a34b23c4c93670e8bc10300409702509b1b59d
Denys Pavlov <me@denys.me>
2025-05-10 00:03:58 -0400

    feat: +flake-check

--- flake-modules/git-hooks.nix
+++ flake-modules/git-hooks.nix
@@ -1,10 +1,17 @@
 { inputs, ... }:
 {
   imports = [ inputs.git-hooks-nix.flakeModule ];
-  perSystem = {
-    pre-commit.settings.hooks = {
-      treefmt.enable = true;
-      commitizen.enable = true;
+  perSystem =
+    { config, ... }:
+    let
+      cfg = config.pre-commit.settings;
+    in
+    {
+      pre-commit.settings.hooks = {
+        treefmt.enable = true;
+        commitizen.enable = true;
+        flake-checker.enable = true;
+        flake-checker.entry = "${cfg.hooks.flake-checker.package}/bin/flake-checker -f --no-telemetry";
+      };
     };
-  };
 }
