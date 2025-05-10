b080f0bac81595876691bd6018488b55097610a7
Denys Pavlov <me@denys.me>
2025-05-09 21:57:22 -0400

    feat: move to nixfmt from alejandra
    prepare for repo-wide reformat

--- flake-modules/treefmt.nix
+++ flake-modules/treefmt.nix
@@ -2,7 +2,7 @@
   imports = [inputs.treefmt-nix.flakeModule];
   perSystem = _: {
     treefmt = {
-      programs.alejandra.enable = true;
+      programs.nixfmt.enable = true;
       programs.statix.enable = true;
       programs.deadnix.enable = true;
       programs.shellcheck.enable = true;
