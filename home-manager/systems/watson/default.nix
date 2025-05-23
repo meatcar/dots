919529c049393ed2b5dbf516eaf4ff9cb1a16e9f
Denys Pavlov <me@denys.me>
2025-05-23 17:26:48 -0400

    fix: run syncthing as a system service

--- home-manager/systems/watson/default.nix
+++ home-manager/systems/watson/default.nix
@@ -54,7 +54,7 @@
 
   programs.chromium.enable = true;
 
-  services.syncthing.enable = true;
+  # services.syncthing.enable = true;
   services.opensnitch-ui.enable = true;
   services.easyeffects.enable = true;
   home.file."/git".source = config.lib.file.mkOutOfStoreSymlink "/git";
