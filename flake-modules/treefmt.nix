7f604c2c1fbcae5077df104aca44f5f263d8c86b
Denys Pavlov <github@denys.me>
2025-09-03 16:28:57 -0400

    feat: add actionlint

--- flake-modules/treefmt.nix
+++ flake-modules/treefmt.nix
@@ -6,11 +6,13 @@
       programs.nixfmt.enable = true;
       programs.statix.enable = true;
       programs.deadnix.enable = true;
+      programs.actionlint.enable = true;
       programs.shfmt.enable = true;
       programs.shellcheck.enable = true;
       programs.shellcheck.excludes = [
         ".envrc"
       ];
+      programs.stylua.enable = true;
     };
   };
 }
