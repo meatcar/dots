f8abb79975a46fce038958399d97e9f03c5431a6
Denys Pavlov <github@denys.me>
2025-09-04 16:15:08 -0400

    refactor: split jjui config into its own module

--- home-manager/modules/jujutsu/default.nix
+++ home-manager/modules/jujutsu/default.nix
@@ -1,20 +1,12 @@
 {
   config,
-  pkgs,
   lib,
   nixpkgs-unstable,
-  inputs,
   ...
 }:
 {
+  imports = [ ./jjui.nix ];
   home.packages = with nixpkgs-unstable; [
-    (pkgs.writeShellScriptBin "jjui" ''
-      # jjui's dep doesn't allow tmux to access background color so lets' just lie to it
-      # see https://github.com/muesli/termenv/blob/2eeba510a727c7211d3797e19294bf7d8859f726/termenv_unix.go#L238
-      # pending on https://github.com/muesli/termenv/pull/123
-      export TERM=$(echo "$TERM" | sed 's/^tmux/xterm/');
-      ${lib.getExe jjui} "$@"
-    '')
     lazyjj
   ];
   programs.jujutsu =
@@ -186,29 +178,4 @@
         ];
       };
     };
-  home.file.".config/jjui/themes".source = "${inputs.tinted-jjui}/themes";
-  xdg.configFile."jjui/config.toml".source = (pkgs.formats.toml { }).generate "config.toml" {
-    ui.theme = {
-      light = "base24-catppuccin-latte";
-      dark = "base24-catppuccin-mocha";
-    };
-    preview = {
-      show_at_start = true;
-    };
-    custom_commands = {
-      delta = {
-        show = "interactive";
-        args = [
-          "util"
-          "exec"
-          "--"
-          "bash"
-          "-c"
-          ''
-            jj show -r $change_id --summary --git --color=always | delta --pager 'less -FRX'
-          ''
-        ];
-      };
-    };
-  };
 }
