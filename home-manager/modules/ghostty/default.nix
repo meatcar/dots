865fa00357622a91807f102c32dd04ff8b456469
Denys Pavlov <github@denys.me>
2025-09-06 14:09:23 -0400

    feat(ghostty): use nixpkgs-unstable package

--- home-manager/modules/ghostty/default.nix
+++ home-manager/modules/ghostty/default.nix
@@ -2,7 +2,7 @@
 {
   programs.ghostty = {
     enable = true;
-    package = specialArgs.inputs.ghostty.packages.${pkgs.stdenv.hostPlatform.system}.default;
+    package = specialArgs.nixpkgs-unstable.ghostty;
     enableBashIntegration = true;
     enableFishIntegration = true;
     settings = {
@@ -10,15 +10,15 @@
       font-size = 10;
       font-feature = "ss07";
       freetype-load-flags = "no-force-autohint"; # let the font hint itself
-      theme = "dark:catppuccin-mocha,light:catppuccin-latte";
+      theme = "dark:Catppuccin Mocha,light:Catppuccin Latte";
       link-url = true;
       window-padding-x = 4;
       window-padding-y = 1;
       window-padding-balance = true;
       window-padding-color = "extend";
       window-theme = "ghostty";
-      gtk-single-instance = true;
       quit-after-last-window-closed = false;
+      shell-integration-features = true;
     };
   };
 }
