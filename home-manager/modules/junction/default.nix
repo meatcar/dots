e9615ae6df2441dc889860bc8a5d046668044398
Denys Pavlov <github@denys.me>
2025-09-06 14:09:23 -0400

    fix(mime): set up default apps in one place

--- home-manager/modules/junction/default.nix
+++ home-manager/modules/junction/default.nix
@@ -4,31 +4,4 @@ let
 in
 {
   home.packages = [ package ];
-  home.sessionVariables = {
-    BROWSER = "${lib.getExe package}";
-  };
-  xdg.mimeApps.defaultApplications = builtins.listToAttrs (
-    map
-      (name: {
-        inherit name;
-        value = "re.sonny.Junction.desktop";
-      })
-      [
-        "application/x-extension-shtml"
-        "application/x-extension-xhtml"
-        "application/x-extension-html"
-        "application/x-extension-xht"
-        "application/x-extension-htm"
-        "x-scheme-handler/unknown"
-        "x-scheme-handler/mailto"
-        "x-scheme-handler/chrome"
-        "x-scheme-handler/about"
-        "x-scheme-handler/https"
-        "x-scheme-handler/http"
-        "application/xhtml+xml"
-        "application/json"
-        # "text/plain"
-        "text/html"
-      ]
-  );
 }
