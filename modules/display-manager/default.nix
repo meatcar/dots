185e03c75edc4e05c19b5d461f9538f500605493
Denys Pavlov <github@denys.me>
2025-09-06 14:09:23 -0400

    feat: enable ly display manager by default

--- modules/display-manager/default.nix
+++ modules/display-manager/default.nix
@@ -1,5 +1,5 @@
 {
   # services.displayManager.enable = true;
-  # services.displayManager.ly.enable = true;
-  services.displayManager.gdm.enable = true;
+  services.displayManager.ly.enable = true;
+  # services.displayManager.gdm.enable = true;
 }
