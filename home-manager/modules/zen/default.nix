93704e2f496c33ee1a1b08e19a698076b1d43246
Denys Pavlov <github@denys.me>
2025-09-06 14:09:23 -0400

    feat(zen): set as default browser

--- home-manager/modules/zen/default.nix
+++ home-manager/modules/zen/default.nix
@@ -1,10 +1,35 @@
-{pkgs, ...}: {
-    programs.zen-browser.nativeMessagingHosts = [pkgs.firefoxpwa];
-    programs.zen-browser = {
-      enable = true;
-      policies = {
-        DisableTelemetry = true;
-        DisableAppUpdate = false;
-      };
+{ pkgs, ... }:
+{
+  programs.zen-browser.nativeMessagingHosts = [ pkgs.firefoxpwa ];
+  programs.zen-browser = {
+    enable = true;
+    policies = {
+      DisableTelemetry = true;
+      DisableAppUpdate = false;
     };
+  };
+  xdg.mimeApps.defaultApplications = builtins.listToAttrs (
+    map
+      (name: {
+        inherit name;
+        value = "zen.desktop";
+      })
+      [
+        "application/x-extension-shtml"
+        "application/x-extension-xhtml"
+        "application/x-extension-html"
+        "application/x-extension-xht"
+        "application/x-extension-htm"
+        "x-scheme-handler/unknown"
+        "x-scheme-handler/mailto"
+        "x-scheme-handler/chrome"
+        "x-scheme-handler/about"
+        "x-scheme-handler/https"
+        "x-scheme-handler/http"
+        "application/xhtml+xml"
+        "application/json"
+        # "text/plain"
+        "text/html"
+      ]
+  );
 }
