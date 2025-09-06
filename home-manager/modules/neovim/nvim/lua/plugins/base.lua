f9653af64e9347e1307057c8ccbf90fa595b460b
Denys Pavlov <github@denys.me>
2025-09-06 14:09:23 -0400

    chore(nvim): lazy update

--- home-manager/modules/neovim/nvim/lua/plugins/base.lua
+++ home-manager/modules/neovim/nvim/lua/plugins/base.lua
@@ -150,7 +150,8 @@ return {
     'ggandor/leap.nvim',
     event = me.o.events.verylazy,
     config = function()
-      require('leap').add_default_mappings()
+      vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap)')
+      vim.keymap.set('n', 'S', '<Plug>(leap-from-window)')
     end,
   },
 
