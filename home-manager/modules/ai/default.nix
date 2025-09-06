c286c39ce44d3ca2fe24eeebbcd9b93bac68aa4a
Denys Pavlov <github@denys.me>
2025-09-06 14:09:23 -0400

    feat: +ampcode

--- home-manager/modules/ai/default.nix
+++ home-manager/modules/ai/default.nix
@@ -1,10 +1,16 @@
-{ config, pkgs, ... }:
+{
+  config,
+  pkgs,
+  nixpkgs-unstable,
+  ...
+}:
 {
   imports = [
     ../aider
     ../opencode
   ];
   home.packages = [
+    nixpkgs-unstable.amp-cli
     (pkgs.writeShellScriptBin "with-aienv" ''
       source ${config.age.secrets.aienv.path}
       "$@"
