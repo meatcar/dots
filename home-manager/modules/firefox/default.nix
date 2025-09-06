2d4e5f8507a23014ca61dac742ffdef865bae87a
Denys Pavlov <github@denys.me>
2025-09-06 14:09:23 -0400

    feat(zen): set as default browser

--- home-manager/modules/firefox/default.nix
+++ home-manager/modules/firefox/default.nix
@@ -22,15 +22,4 @@
     recursive = true;
   };
 
-  xdg.mimeApps.defaultApplications =
-    let
-      browser = "firefox.desktop";
-    in
-    {
-      "text/html" = browser;
-      "x-scheme-handler/http" = browser;
-      "x-scheme-handler/https" = browser;
-      "x-scheme-handler/about" = browser;
-      "x-scheme-handler/unknown" = browser;
-    };
 }
