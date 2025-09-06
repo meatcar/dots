c7819fd1eaad15c41eed80fe18df2aa3d5576598
Denys Pavlov <github@denys.me>
2025-09-06 14:09:23 -0400

    feat(tv): enable nix-search-tv

--- home-manager/modules/television/default.nix
+++ home-manager/modules/television/default.nix
@@ -1,10 +1,26 @@
+{ config, pkgs, ... }:
 {
+  programs.nix-search-tv.enable = true;
   programs.television = {
     enable = true;
     settings = {
       ui = {
         use_nerd_font_icons = true;
       };
+      shell_integration.channel_triggers.zoxide = [ "z" ];
     };
+    channels.git-dirs = {
+      metadata = {
+        description = "A channel to select git directories";
+        name = "zoxide";
+      };
+      preview = {
+        command = "ls -la '{0}'";
+      };
+      source = {
+        command = "${pkgs.zoxide}/bin/zoxide query --list";
+      };
+    };
+    enableFishIntegration = config.programs.fish.enable;
   };
 }
