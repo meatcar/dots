8cc469c3486101acdc127b771953ff54325db9d9
Denys Pavlov <github@denys.me>
2025-09-06 14:09:23 -0400

    feat(1password): switch to latest since wayland support dropped
    see https://www.reddit.com/r/1Password/comments/1lut96p/new_in_1password_for_linux_wayland_clipboard/

--- modules/1password/default.nix
+++ modules/1password/default.nix
@@ -1,7 +1,10 @@
-_: {
+{ nixpkgs-unstable, ... }:
+{
   programs._1password.enable = true;
+  programs._1password.package = nixpkgs-unstable._1password;
   programs._1password-gui = {
     enable = true;
+    package = nixpkgs-unstable._1password-gui;
     # Certain features, including CLI integration and system authentication support,
     # require enabling PolKit integration on some desktop environments (e.g. Plasma).
     polkitPolicyOwners = [ "meatcar" ];
