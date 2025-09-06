5a15d5779e2552e649b6e0fe79a95e170346c47c
Denys Pavlov <github@denys.me>
2025-09-06 14:09:23 -0400

    feat: enable comma

--- home-manager/modules/nix-index/default.nix
+++ home-manager/modules/nix-index/default.nix
@@ -7,6 +7,5 @@
     inputs.nix-index-database.homeModules.nix-index
   ];
   programs.nix-index.enable = true;
-  # TODO: fixes https://github.com/nix-community/nix-index-database/issues/134
-  # programs.nix-index-database.comma.enable = true;
+  programs.nix-index-database.comma.enable = true;
 }
