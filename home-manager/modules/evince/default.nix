d7557bdc2d1d8e7bc99458d72fab6af15585796e
Denys Pavlov <github@denys.me>
2025-09-06 14:09:23 -0400

    fix(mime): set up default apps in one place

--- home-manager/modules/evince/default.nix
+++ home-manager/modules/evince/default.nix
@@ -2,8 +2,6 @@
 {
   home.packages = [
     pkgs.evince
+    pkgs.papers
   ];
-  xdg.mimeApps.defaultApplications = {
-    "application/pdf" = "org.gnome.Evince.desktop";
-  };
 }
---
+++ home-manager/modules/mime-apps/default.nix
@@ -0,0 +1,39 @@
+{ pkgs, ... }:
+{
+
+  home.sessionVariables = {
+    BROWSER = "dms open";
+  };
+  xdg.mimeApps.defaultApplications =
+    let
+      dms = "dms-open.desktop";
+      zed = "dev.zed.Zed.desktop";
+      evince = "org.gnome.Evince.desktop";
+      papers = "org.gnome.Papers.desktop";
+    in
+    {
+      "application/x-extension-shtml" = dms;
+      "application/x-extension-xhtml" = dms;
+      "application/x-extension-html" = dms;
+      "application/x-extension-xht" = dms;
+      "application/x-extension-htm" = dms;
+      "x-scheme-handler/unknown" = dms;
+      "x-scheme-handler/mailto" = dms;
+      "x-scheme-handler/chrome" = dms;
+      "x-scheme-handler/about" = dms;
+      "x-scheme-handler/https" = dms;
+      "x-scheme-handler/http" = dms;
+      "application/xhtml+xml" = dms;
+      "application/json" = dms;
+      "text/html" = dms;
+      "application/pdf" = papers;
+    };
+  home.packages = [
+    (pkgs.writeShellScriptBin "mime-apps-list" ''
+      ls /run/current-system/sw/share/applications # for global packages
+      ls /etc/profiles/per-user/$(id -n -u)/share/applications # for user packages
+      ls ~/.nix-profile/share/applications # for home-manager packages
+    '')
+    pkgs.shared-mime-info
+  ];
+}
