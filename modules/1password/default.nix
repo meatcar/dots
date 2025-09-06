0eb60ce1bcd602b0360dd2d25fd5c526b46ab42d
Denys Pavlov <github@denys.me>
2025-09-06 14:09:23 -0400

    fix(1password): use latest package names and support zen browser

--- modules/1password/default.nix
+++ modules/1password/default.nix
@@ -1,12 +1,22 @@
 { nixpkgs-unstable, ... }:
 {
-  programs._1password.enable = true;
-  programs._1password.package = nixpkgs-unstable._1password;
+  programs._1password = {
+    enable = true;
+    package = nixpkgs-unstable._1password-cli;
+  };
   programs._1password-gui = {
     enable = true;
     package = nixpkgs-unstable._1password-gui;
     # Certain features, including CLI integration and system authentication support,
     # require enabling PolKit integration on some desktop environments (e.g. Plasma).
     polkitPolicyOwners = [ "meatcar" ];
+  };
+  environment.etc = {
+    "1password/custom_allowed_browsers" = {
+      text = ''
+        zen
+      ''; # or just "zen" if you use unwrapped package
+      mode = "0755";
+    };
   };
 }
