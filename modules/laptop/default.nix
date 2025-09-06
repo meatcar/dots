e9a9a1ddf1176481de37457780b3f03f867764fc
Denys Pavlov <github@denys.me>
2025-09-06 14:09:23 -0400

    feat(laptop): add performance-improving kernel params

--- modules/laptop/default.nix
+++ modules/laptop/default.nix
@@ -18,4 +18,11 @@
     PowerKeyLongPress = "poweroff";
   };
 
+  boot.kernelParams = [
+    # from https://discourse.ubuntu.com/t/fine-tuning-the-ubuntu-24-04-kernel-for-low-latency-throughput-and-power-efficiency/44834
+    "preempt=full"
+    "rcu_nocbs=all"
+    "rcutree.enable_rcu_lazy=1" # see https://wiki.cachyos.org/configuration/general_system_tweaks/#enable-rcu-lazy
+  ];
+
 }
