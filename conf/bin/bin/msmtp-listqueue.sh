5f6de83633258e1fd758bd6cc81a45b004293226
Denys Pavlov <github@denys.me>
2026-01-24 23:08:30 -0500

    chore: remove old unused files

--- conf/bin/bin/msmtp-listqueue.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-QUEUEDIR=${QUEUEDIR:-$HOME/.msmtpqueue}
-
-for i in "$QUEUEDIR"/*.mail; do
-  grep -Es --colour -h '(^From:|^To:|^Subject:)' "$i" || echo "No mail in queue"
-  echo " "
-done
--- conf/nix/.config/nixpkgs/config.nix
+++
@@ -1,1 +0,0 @@
-../../../../home-manager/config.nix
