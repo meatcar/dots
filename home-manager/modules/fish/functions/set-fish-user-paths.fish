da9f1e01943fe0437da8fb15ba0a1187f8c998f0
Denys Pavlov <github@denys.me>
2026-02-19 12:43:30 -0500

    fix(fish): fix fish_user_paths, add bun

--- home-manager/modules/fish/functions/set-fish-user-paths.fish
+++ home-manager/modules/fish/functions/set-fish-user-paths.fish
@@ -1,42 +1,41 @@
-function addpath
-    set -aU fish_user_paths $argv[1]
-end
-
 function command_exists
-    # make sure command is not a windows executable
-    set -l fullpath (command -s $argv[1])
-    test -n $fullpath && string match -q -r '^/mnt/c' $fullpath
+  # make sure command is not a windows executable
+  set -l fullpath (command -s $argv[1])
+  test -n $fullpath && string match -q --invert -r '^/mnt/c' $fullpath
 end
 
 function set-fish-user-paths \
-    --description "Set fish_user_paths"
+  --description "Set fish_user_paths"
 
-    set -eU fish_user_paths
+  set -eU fish_user_paths
 
-    if [ -d $HOME/bin ]
-        addpath ~/bin
-    end
+  if [ -d $HOME/bin ]
+    fish_add_path ~/bin
+  end
 
-    if command_exists emacs
-        addpath $HOME/.emacs.d/bin
-    end
+  if [ -d $HOME/.local/bin ]
+    fish_add_path $HOME/.local/bin
+  end
 
-    if [ -d $HOME/.local/bin ]
-        addpath $HOME/.local/bin
-    end
+  if command_exists emacs
+    fish_add_path $HOME/.emacs.d/bin
+  end
 
-    if command_exists yarn
-        addpath (yarn global bin)
-    end
+  if command_exists yarn
+    fish_add_path (yarn global bin)
+  end
 
-    if command_exists npm
-        addpath (npm bin -g)
-    end
+  if command_exists npm
+    fish_add_path (npm bin -g)
+  end
 
-    if command_exists go
-        set -x GOPATH $HOME/go
-        addpath (go env GOROOT)/bin
-        addpath (go env GOPATH)/bin
-    end
+  if command_exists bun
+    fish_add_path (bun pm bin -g)
+  end
+
+  if command_exists go
+    set -x GOPATH $HOME/go
+    fish_add_path (go env GOROOT)/bin
+    fish_add_path (go env GOPATH)/bin
+  end
 end
-
