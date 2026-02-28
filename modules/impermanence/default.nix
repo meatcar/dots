3144e800df23455299b44ef4c93ea1531c1e083e
Denys Pavlov <github@denys.me>
2026-02-27 20:18:24 -0500

    fix: secrets config

--- modules/impermanence/default.nix
+++ modules/impermanence/default.nix
@@ -128,6 +128,10 @@ in
 
     files = [
       "/etc/machine-id"
+      "/etc/ssh/ssh_host_ed25519_key"
+      "/etc/ssh/ssh_host_ed25519_key.pub"
+      "/etc/ssh/ssh_host_rsa_key"
+      "/etc/ssh/ssh_host_rsa_key.pub"
     ];
 
     hideMounts = true;
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,20 +2,24 @@ let
   mormont = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBcq01gh2tn/+hcm75N3LnS003mUBjXcT6qNndMhObPO";
   watson = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOlUHaqIExPCm99pWSUhsi2O2ic3KwRJ/VUmJ28b2xNM meatcar@watson";
   ci = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHtmRcvR5Ron/AnnOhksodO+8ZSYTfM3RtURqnQWTrTw meatcar@ci";
-  systems = [
+  users = [
     mormont
     watson
     ci
   ];
+  watsonHost = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIk7JFP7cWKnFJbpdzvMc6M9Iacbh4buNHepENe1IrSl root@watson";
+  systems = users ++ [
+    watsonHost
+  ];
 in
 {
-  "aienv.age".publicKeys = systems;
-  "pushover.age".publicKeys = systems;
+  "aienv.age".publicKeys = users;
+  "pushover.age".publicKeys = users;
   # https://console.cloud.google.com/auth/clients/create
-  "gcalClientId.age".publicKeys = systems;
-  "gcalClientSecret.age".publicKeys = systems;
-  "spotifyClientId.age".publicKeys = systems;
-  "spotifyClientSecret.age".publicKeys = systems;
-  "cliProxyApiEnv.age".publicKeys = systems;
+  "gcalClientId.age".publicKeys = users;
+  "gcalClientSecret.age".publicKeys = users;
+  "spotifyClientId.age".publicKeys = users;
+  "spotifyClientSecret.age".publicKeys = users;
+  "cliProxyApiEnv.age".publicKeys = users;
   "userPassword.age".publicKeys = systems;
 }
