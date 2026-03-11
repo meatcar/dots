b5890963201e48ab1d9141842eb2f657950ea8ec
Denys Pavlov <github@denys.me>
2026-03-11 18:04:12 -0400

    feat(dms): use latest quickshell

--- home-manager/modules/dms/default.nix
+++ home-manager/modules/dms/default.nix
@@ -1,4 +1,5 @@
 {
+  config,
   pkgs,
   lib,
   inputs,
@@ -6,16 +7,20 @@
   ...
 }:
 let
+  cfg = config.programs.dank-material-shell;
   dms = lib.getExe inputs.dank-material-shell.packages.${pkgs.stdenv.hostPlatform.system}.dms-shell;
-  pathPrefix = "PATH=${lib.makeBinPath [ nixpkgs-unstable.quickshell ]}:$PATH";
+  pathPrefix = "PATH=${lib.makeBinPath [ cfg.quickshell.package ]}:$PATH";
 in
 {
   programs.dank-material-shell = {
     enable = true;
     systemd.enable = true;
-    quickshell.package = nixpkgs-unstable.quickshell;
+    # quickshell.package = nixpkgs-unstable.quickshell;
     dgop.package = nixpkgs-unstable.dgop;
   };
+  home.packages = with pkgs; [
+    kdePackages.kimageformats
+  ];
   services.darkman = {
     darkModeScripts = {
       dms = ''
--- home-manager/modules/neovim/nvim/lua/plugins/dankcolors.lua
+++ home-manager/modules/neovim/nvim/lua/plugins/dankcolors.lua
@@ -4,38 +4,38 @@ return {
 		priority = 1000,
 		config = function()
 			require('base16-colorscheme').setup({
-				base00 = '#0f1417',
-				base01 = '#0f1417',
-				base02 = '#939a9f',
-				base03 = '#939a9f',
-				base04 = '#eff8ff',
-				base05 = '#f8fcff',
-				base06 = '#f8fcff',
-				base07 = '#f8fcff',
-				base08 = '#ff9fbd',
-				base09 = '#ff9fbd',
-				base0A = '#a7dcff',
-				base0B = '#a5ffb0',
-				base0C = '#d0ecff',
-				base0D = '#a7dcff',
-				base0E = '#b6e2ff',
-				base0F = '#b6e2ff',
+				base00 = '#111318',
+				base01 = '#111318',
+				base02 = '#77797b',
+				base03 = '#77797b',
+				base04 = '#262728',
+				base05 = '#d4d6d9',
+				base06 = '#d4d6d9',
+				base07 = '#d4d6d9',
+				base08 = '#ad516b',
+				base09 = '#ad516b',
+				base0A = '#4f6eaf',
+				base0B = '#429f50',
+				base0C = '#8a9bbc',
+				base0D = '#4f6eaf',
+				base0E = '#d3e2ff',
+				base0F = '#d3e2ff',
 			})
 
 			vim.api.nvim_set_hl(0, 'Visual', {
-				bg = '#939a9f',
-				fg = '#f8fcff',
+				bg = '#77797b',
+				fg = '#d4d6d9',
 				bold = true
 			})
 			vim.api.nvim_set_hl(0, 'Statusline', {
-				bg = '#a7dcff',
-				fg = '#0f1417',
+				bg = '#4f6eaf',
+				fg = '#111318',
 			})
-			vim.api.nvim_set_hl(0, 'LineNr', { fg = '#939a9f' })
-			vim.api.nvim_set_hl(0, 'CursorLineNr', { fg = '#d0ecff', bold = true })
+			vim.api.nvim_set_hl(0, 'LineNr', { fg = '#77797b' })
+			vim.api.nvim_set_hl(0, 'CursorLineNr', { fg = '#8a9bbc', bold = true })
 
 			vim.api.nvim_set_hl(0, 'Statement', {
-				fg = '#b6e2ff',
+				fg = '#d3e2ff',
 				bold = true
 			})
 			vim.api.nvim_set_hl(0, 'Keyword', { link = 'Statement' })
@@ -43,34 +43,34 @@ return {
 			vim.api.nvim_set_hl(0, 'Conditional', { link = 'Statement' })
 
 			vim.api.nvim_set_hl(0, 'Function', {
-				fg = '#a7dcff',
+				fg = '#4f6eaf',
 				bold = true
 			})
 			vim.api.nvim_set_hl(0, 'Macro', {
-				fg = '#a7dcff',
+				fg = '#4f6eaf',
 				italic = true
 			})
 			vim.api.nvim_set_hl(0, '@function.macro', { link = 'Macro' })
 
 			vim.api.nvim_set_hl(0, 'Type', {
-				fg = '#d0ecff',
+				fg = '#8a9bbc',
 				bold = true,
 				italic = true
 			})
 			vim.api.nvim_set_hl(0, 'Structure', { link = 'Type' })
 
 			vim.api.nvim_set_hl(0, 'String', {
-				fg = '#a5ffb0',
+				fg = '#429f50',
 				italic = true
 			})
 
-			vim.api.nvim_set_hl(0, 'Operator', { fg = '#eff8ff' })
-			vim.api.nvim_set_hl(0, 'Delimiter', { fg = '#eff8ff' })
+			vim.api.nvim_set_hl(0, 'Operator', { fg = '#262728' })
+			vim.api.nvim_set_hl(0, 'Delimiter', { fg = '#262728' })
 			vim.api.nvim_set_hl(0, '@punctuation.bracket', { link = 'Delimiter' })
 			vim.api.nvim_set_hl(0, '@punctuation.delimiter', { link = 'Delimiter' })
 
 			vim.api.nvim_set_hl(0, 'Comment', {
-				fg = '#939a9f',
+				fg = '#77797b',
 				italic = true
 			})
 
