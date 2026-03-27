d083226208414ce15fdca893c5c46b6649d043a8
Denys Pavlov <github@denys.me>
2026-03-27 00:27:38 -0400

    feat(treefmt): +oxfmt

--- flake-modules/treefmt.nix
+++ flake-modules/treefmt.nix
@@ -1,17 +1,28 @@
 { inputs, ... }:
 {
   imports = [ inputs.treefmt-nix.flakeModule ];
-  perSystem = {
-    treefmt = {
-      programs.nixfmt.enable = true;
-      programs.statix.enable = true;
-      programs.deadnix.enable = true;
-      programs.actionlint.enable = true;
-      programs.shfmt.enable = true;
-      programs.shellcheck.enable = true;
-      programs.shellcheck.excludes = [
-        ".envrc"
-      ];
+  perSystem =
+    { inputs', ... }:
+    let
+      pkgsUnstable = inputs'.nixpkgs-unstable.legacyPackages;
+    in
+    {
+      treefmt = {
+        programs.nixfmt.enable = true;
+        programs.statix.enable = true;
+        programs.deadnix.enable = true;
+        programs.actionlint.enable = true;
+        programs.oxfmt = {
+          enable = true;
+          package = pkgsUnstable.oxfmt;
+        };
+        programs.shfmt.enable = true;
+        programs.shellcheck = {
+          enable = true;
+          excludes = [
+            ".envrc"
+          ];
+        };
+      };
     };
-  };
 }
