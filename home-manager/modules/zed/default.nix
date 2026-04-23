efa5336e4b72c2fa8722ea1ee5e7e5218ab3e87c
Denys Pavlov <github@denys.me>
2026-04-22 21:28:33 -0400

    chore(zed): update to v0.233.6

--- home-manager/modules/zed/default.nix
+++ home-manager/modules/zed/default.nix
@@ -5,9 +5,25 @@
   nixpkgs-unstable,
   ...
 }:
+let
+  version = "0.233.6";
+  src = nixpkgs-unstable.fetchFromGitHub {
+    owner = "zed-industries";
+    repo = "zed";
+    tag = "v${version}";
+    hash = "sha256-VdCnZpNvjv9Soldpz7ZlnI6Lp6uFZqF6zVVo4+jcu/o=";
+  };
+in
 {
   programs.zed-editor.enable = true;
-  programs.zed-editor.package = nixpkgs-unstable.zed-editor;
+  programs.zed-editor.package = nixpkgs-unstable.zed-editor.overrideAttrs (_: {
+    inherit version src;
+    cargoDeps = nixpkgs-unstable.rustPlatform.fetchCargoVendor {
+      inherit src;
+      hash = "sha256-aKVal37mCPgKBdYBgmUZZ3F6zBzUu85IsvLECK4+Ldg=";
+      postBuild = "rm -rf $out/git/*/candle-book/";
+    };
+  });
   # some programs assume zeditor is zed
   home.packages = [
     (pkgs.writeShellScriptBin "zed" ''
