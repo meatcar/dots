93a06fa1c6b2899a807ed3c7ca24c13bb86175d1
Denys Pavlov <github@denys.me>
2026-04-25 17:07:43 -0400

    fix(niri): remove ScreenCast/Screenshot/RemoteDesktop=gnome from portal config

--- home-manager/modules/niri/default.nix
+++ home-manager/modules/niri/default.nix
@@ -84,10 +84,9 @@ in
   dbus.packages = [ pkgs.nautilus ];
   xdg.configFile."xdg-desktop-portal/niri-portals.conf".text = ''
     [preferred]
-    default=gtk
-    org.freedesktop.impl.portal.ScreenCast=gnome
-    org.freedesktop.impl.portal.Screenshot=gnome
-    org.freedesktop.impl.portal.RemoteDesktop=gnome
+    default=gnome;gtk
+    org.freedesktop.impl.portal.Access=gtk
+    org.freedesktop.impl.portal.Notification=gtk
     org.freedesktop.impl.portal.Settings=gnome
     org.freedesktop.impl.portal.Secret=gnome-keyring
   '';
