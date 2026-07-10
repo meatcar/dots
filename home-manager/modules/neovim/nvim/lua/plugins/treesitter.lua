f053997019dfb128df1589cd1886d047bf0273e1
Denys Pavlov <github@denys.me>
2026-07-10 01:13:12 -0400

    refactor(neovim): migrate nvim-treesitter and textobjects to main branch

--- home-manager/modules/neovim/nvim/lua/plugins/treesitter.lua
+++ home-manager/modules/neovim/nvim/lua/plugins/treesitter.lua
@@ -1,16 +1,53 @@
 return {
-  { -- nvim-treesitter/nvim-treesitter, installed through nixos
+  { -- nvim-treesitter/nvim-treesitter (main branch: no lazy-load, no configs.setup)
     'nvim-treesitter/nvim-treesitter',
     name = 'nvim-treesitter',
-    event = me.o.events.buf_late,
+    branch = 'main',
+    lazy = false,
+    build = ':TSUpdate',
     config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('nvim-treesitter.configs').setup {
-        highlight = { enable = true },
-        indent = { enable = true },
+      -- parsers we commonly edit; installed async, idempotent
+      require('nvim-treesitter').install {
+        'bash', 'c', 'comment', 'css', 'diff', 'dockerfile', 'eex', 'elixir',
+        'fish', 'git_rebase', 'gitcommit', 'go', 'gomod', 'hcl', 'heex', 'html',
+        'javascript', 'json', 'lua', 'luadoc', 'markdown', 'markdown_inline',
+        'nix', 'python', 'query', 'regex', 'ruby', 'rust', 'teal', 'terraform',
+        'toml', 'tsx', 'typescript', 'vim', 'vimdoc', 'vue', 'yaml',
       }
+
+      -- main has no `highlight.enable`; start per-buffer on FileType and set the
+      -- treesitter indentexpr. Auto-install missing-but-available parsers, guarded
+      -- with pcall + err check to avoid the picker race upstream warns about.
+      vim.api.nvim_create_autocmd('FileType', {
+        group = vim.api.nvim_create_augroup('me_treesitter', { clear = true }),
+        desc = 'treesitter highlight + indent',
+        callback = function(ev)
+          local buf = ev.buf
+          local ft = vim.bo[buf].filetype
+          if ft == '' then return end
+          local lang = vim.treesitter.language.get_lang(ft) or ft
+
+          local function enable()
+            if not vim.api.nvim_buf_is_valid(buf) then return end
+            if pcall(vim.treesitter.start, buf, lang) then
+              vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+            end
+          end
+
+          local ts = require('nvim-treesitter')
+          if vim.list_contains(ts.get_installed('parsers'), lang) then
+            enable()
+          elseif vim.list_contains(ts.get_available(), lang) then
+            ts.install(lang):await(function(err)
+              if not err then vim.schedule(enable) end
+            end)
+          end
+        end,
+      })
+
+      -- folding via core treesitter (ufo consumes the same folds.scm queries)
       vim.o.foldmethod = 'expr'
-      vim.o.foldexpr = 'nvim_treesitter#foldexpr()'
+      vim.o.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
     end,
   },
 
@@ -30,56 +67,51 @@ return {
     end
   },
 
-  -- Syntax aware text-objects, select, move, swap, and peek support
+  -- Syntax aware text-objects: select, move, swap (main branch)
+  -- note: lsp_interop/peek_definition_code was removed upstream; <leader>df/dF are gone
   {
     'nvim-treesitter/nvim-treesitter-textobjects',
+    branch = 'main',
     event = me.o.events.buf_late,
+    init = function()
+      vim.g.no_plugin_maps = true -- disable built-in ftplugin maps that may conflict
+    end,
     config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('nvim-treesitter.configs').setup {
-        textobjects = {
-          select = {
-            enable = true,
-            -- Automatically jump forward to textobj, similar to targets.vim
-            lookahead = true,
-
-            keymaps = {
-              -- You can use the capture groups defined in textobjects.scm
-              ['af'] = '@function.outer',
-              ['if'] = '@function.inner',
-              ['ac'] = '@class.outer',
-              ['ic'] = '@class.inner',
-              ['aa'] = '@parameter.outer',
-              ['ia'] = '@parameter.inner',
-              ['ak'] = '@block.outer',
-              ['ik'] = '@block.inner',
-            },
-            -- You can choose the select mode (default is charwise 'v')
-            selection_modes = {
-              ['@parameter.outer'] = 'v', -- charwise
-              ['@function.outer'] = 'V',  -- linewise
-              ['@class.outer'] = '<c-v>', -- blockwise
-            },
-          },
-          swap = {
-            enable = true,
-            swap_next = {
-              ['<leader>a'] = '@parameter.inner',
-            },
-            swap_previous = {
-              ['<leader>A'] = '@parameter.inner',
-            },
-          },
-          lsp_interop = {
-            enable = true,
-            border = 'none',
-            peek_definition_code = {
-              ['<leader>df'] = '@function.outer',
-              ['<leader>dF'] = '@class.outer',
-            },
+      require('nvim-treesitter-textobjects').setup {
+        select = {
+          -- Automatically jump forward to textobj, similar to targets.vim
+          lookahead = true,
+          selection_modes = {
+            ['@parameter.outer'] = 'v', -- charwise
+            ['@function.outer'] = 'V',  -- linewise
+            ['@class.outer'] = '<c-v>', -- blockwise
           },
         },
+        move = { set_jumps = true },
       }
+
+      local select = require('nvim-treesitter-textobjects.select')
+      local function sel(lhs, obj)
+        vim.keymap.set({ 'x', 'o' }, lhs, function()
+          select.select_textobject(obj, 'textobjects')
+        end, { desc = 'select ' .. obj })
+      end
+      sel('af', '@function.outer')
+      sel('if', '@function.inner')
+      sel('ac', '@class.outer')
+      sel('ic', '@class.inner')
+      sel('aa', '@parameter.outer')
+      sel('ia', '@parameter.inner')
+      sel('ak', '@block.outer')
+      sel('ik', '@block.inner')
+
+      local swap = require('nvim-treesitter-textobjects.swap')
+      vim.keymap.set('n', '<leader>a', function()
+        swap.swap_next('@parameter.inner')
+      end, { desc = 'swap next parameter' })
+      vim.keymap.set('n', '<leader>A', function()
+        swap.swap_previous('@parameter.inner')
+      end, { desc = 'swap previous parameter' })
     end,
   },
   { -- close function blocks
